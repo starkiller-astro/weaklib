5aabb356d470db99cb13797b6c26461a78489a85
Ryan Landfield <ryanlandfield@gmail.com>
2014-11-19 20:17:36 +0000

    Minor changes

--- EosFieldsModule.f90
+++ EosFieldsModule.f90
@@ -51,10 +51,11 @@ INTEGER(HID_T), PUBLIC     :: file_id               ! HDF5 File identifier
 INTEGER(HID_T), PUBLIC     :: dataset_id            ! HDF5 dataset identifier
 INTEGER(HID_T), PUBLIC     :: dataspace_id          ! HDF5 dataspace identifier
 INTEGER                    :: hdferr                ! Error flag
-INTEGER(HSIZE_T), ALLOCATABLE, DIMENSION(1:3) ::&
-            dims = (/dim0, dim1, dim2/)             ! Size EOS read buffers
-INTEGER(HSIZE_T), ALLOCATABLE, DIMENSION(1:4) ::&
-            bdims = (/dim3, dim0, dim1, dim2/)      ! size Opacity read buffer
+INTEGER, PARAMETER    :: dim4     = 1          ! # of metadata values
+INTEGER, PARAMETER    :: dim5     = 15         ! # of metadata values
+INTEGER(HSIZE_T), ALLOCATABLE, DIMENSION(1:3) :: dims(:,:,:) ! = (/dim0, dim1, dim2/)             ! Size EOS read buffers
+!INTEGER(HSIZE_T), ALLOCATABLE, DIMENSION(1:4) ::&
+!            bdims = (/dim3, dim0, dim1, dim2/)      ! size Opacity read buffer
 INTEGER(HSIZE_T), DIMENSION(1:2)   ::&
             mddims = (/dim4, dim5/)                 ! size metadata write buffer
 
@@ -73,33 +74,38 @@ CONTAINS
     CALL h5open_f(hdferr)
     CALL h5fopen_f(filename, H5F_ACC_RDONLY, file_id, hdferr);
     CALL h5dopen_f(file_id, "metadata", dataset_id, hdferr)
-    CALL h5dread_f(dataset_id, H5T_NATIVE_DOUBLE, MetaData(:,1), mddims, hdferr)
+    CALL h5dread_f(dataset_id, H5T_NATIVE_DOUBLE, Metadata(:,:), mddims, hdferr)
     CALL h5dclose_f(dataset_id, hdferr)
-   metadata(1,1) = Nrho
-   metadata(1,2) = Nt
-   metadata(1,3) = Nye
-   metadata(1,4) = rhomin ! Min rho
-   metadata(1,5) = rhomax !Max rho
-   metadata(1,6) = Tmin   !Min T  
-   metadata(1,7) = Tmax   !Max T  
-   metadata(1,8) = Yemin  !Min Ye  
-   metadata(1,9) = Yemax  !Max Ye
-   metadata(1,10) = 12.d0    ! Number of EOS fields
-   metadata(1,11) = 2.d0     !Number of Opacity fields
-   metadata(1,12) = 1.d0     !Number of Neutrino Species
-   metadata(1,13) = nez   !Number of Energy groups
-   metadata(1,14) = Emin
-   metadata(1,15) = Emax
 
+      ALLOCATE(dims(Metadata(1,1,),Metadata(1,2),Metadata(1,3))
+!   Metadata(1,1) = Nrho
+!   Metadata(1,2) = Nt
+!   Metadata(1,3) = Nye
+!   Metadata(1,4) = rhomin ! Min rho
+!   Metadata(1,5) = rhomax !Max rho
+!   Metadata(1,6) = Tmin   !Min T  
+!   Metadata(1,7) = Tmax   !Max T  
+!   Metadata(1,8) = Yemin  !Min Ye  
+!   Metadata(1,9) = Yemax  !Max Ye
+!   Metadata(1,10) = 12.d0    ! Number of EOS fields
+!   Metadata(1,11) = 2.d0     !Number of Opacity fields
+!   Metadata(1,12) = 1.d0     !Number of Neutrino Species
+!   Metadata(1,13) = nezbuff   !Number of Energy groups
+!   Metadata(1,14) = Emin
+!   Metadata(1,15) = Emax
   End Subroutine ReadEosMetadata
   
+
+!  Subroutine AllocateRhoTYeFields
+!  End Subroutine AllocateRhoTYeFields
+  
+
   Subroutine AllocateEosFields
   !-------------------------------------------------------------
   ! Initialize HDF5 FORTRAN interface.
   !-------------------------------------------------------------
     CALL h5open_f(filename, H5F_ACC_RDONLY, file_id, hdferr)
-      ALLOCATE(GridData( 
-      ALLOCATE(EosData(Nrho,Nt,Nye,N_EOS_FIELDS))
+      ALLOCATE(EosData(Metadata(1,1,),Metadata(1,2),Metadata(1,3),N_EOS_FIELDS))
 
 
   End Subroutine AllocateEosFields
