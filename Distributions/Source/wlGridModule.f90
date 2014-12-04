dd888cab90fae6b61693048e8bb58c833c83170d
Ryan Landfield <ryanlandfield@gmail.com>
2014-12-04 22:56:39 +0000

    Created wlGridModule, and used successfully in the UnitTest (wlStateModuleTest)

---
+++ Distributions/Source/wlGridModule.f90
@@ -0,0 +1,60 @@
+MODULE wlGridModule
+
+  USE wlKindModule, ONLY: dp
+
+  implicit none
+  PRIVATE
+
+  PUBLIC MakeLinearGrid
+  PUBLIC MakeLogGrid
+
+CONTAINS 
+
+  SUBROUTINE MakeLinearGrid( LowerBound, UpperBound, nPoints, Grid )
+
+    Integer, INTENT(in)              :: nPoints  
+    REAL(dp), INTENT(in)              :: LowerBound
+    REAL(dp), INTENT(in)              :: UpperBound 
+   
+    REAl(dp), DIMENSION(nPoints), INTENT(out)   :: Grid 
+
+    Integer  :: m
+    REAL(dp) :: BinWidth
+
+    BinWidth = ( UpperBound - LowerBound ) / DBLE( nPoints - 1 )
+
+    Grid(1) = LowerBound
+
+    DO m=2,nPoints - 1
+      Grid(m) = Grid(m-1) + BinWidth
+    END DO
+
+    Grid(nPoints) = UpperBound
+
+  END SUBROUTINE MakeLinearGrid
+
+  SUBROUTINE MakeLogGrid( LowerBound, UpperBound, nPoints, Grid )
+
+    Integer, INTENT(in)      :: nPoints          
+    REAL(dp), INTENT(in)     :: LowerBound
+    Real(dp), INTENT(in)     :: UpperBound
+
+    REAl(dp), DIMENSION(nPoints), INTENT(out)   :: Grid 
+
+    Integer  :: m
+    REAL(dp) :: ScaleFactor
+
+    ScaleFactor =  EXP( LOG( UpperBound / LowerBound ) / DBLE( nPoints - 1 ) )
+
+    Grid(1) = LowerBound
+
+    DO m=2,nPoints - 1
+      Grid(m) = Grid(m-1) * ScaleFactor
+    END DO
+
+    Grid(nPoints) = UpperBound
+
+  END SUBROUTINE MakeLogGrid
+
+
+END MODULE wlGridModule
--- Utilities/UnitTests/ThermoState/wlThermoStateTest.f90
+++ Utilities/UnitTests/ThermoState/wlThermoStateTest.f90
@@ -2,6 +2,7 @@ PROGRAM wlThermoStateTest
 
   USE wlThermoStateModule
   USE wlKindModule, ONLY: dp
+  USE wlGridModule, ONLY: MakeLinearGrid, MakeLogGrid
 
   implicit none
 
@@ -9,38 +10,35 @@ PROGRAM wlThermoStateTest
   INTEGER, DIMENSION(3) :: npts
   INTEGER :: nvar
   INTEGER :: j   
-  TYPE(ThermoStateType) :: State1
+  TYPE(ThermoStateType) :: ThermoState
 
-  npts = (/2,2,2/)
+  npts = (/10,31,61/)
 
 
-  Call AllocateThermoState( State1, npts )
+  Call AllocateThermoState( ThermoState, npts )
 
-  State1 % nValues(1:3) = npts(1:3)
-  State1 % Names(1:3) = (/'Density                         ',&
+  ThermoState % nValues(1:3) = npts(1:3)
+  ThermoState % Names(1:3) = (/'Density                         ',&
                           'Temperature                     ',&
                           'Electron Fraction               '/)
     
-  State1 % minValues(1:3) =  (/1.0d06,0.1d00,1.0d-02/)
-  State1 % maxValues(1:3) =  (/1.0d15,5.0d01,6.1d-01/)
+  ThermoState % minValues(1:3) =  (/1.0d06,0.1d00,1.0d-02/)
+  ThermoState % maxValues(1:3) =  (/1.0d15,1.0d02,6.1d-01/)
 
-  DO nvar = 1,3
-    DO j = 1,State1 % nValues(nvar)
-      State1 % States(nvar)%Values(1:j) = State1%minValues(1:j) + (10.d0**((DBLE(j-1)/DBLE(State1%nValues(1:j)))*LOG10(State1%maxValues(1:j)-State1%minValues(1:j)))) 
-    END DO
+  CALL MakeLogGrid( ThermoState%minValues(1), ThermoState%maxValues(1),&
+         ThermoState%nValues(1), ThermoState%States(1)%Values)
+  CALL MakeLogGrid( ThermoState%minValues(2), ThermoState%maxValues(2),&
+         ThermoState%nValues(2), ThermoState%States(2)%Values)
+  CALL MakeLinearGrid( ThermoState%minValues(3), ThermoState%maxValues(3),&
+         ThermoState%nValues(3), ThermoState%States(3)%Values)
+
+  DO j = 1,3
+    WRITE(*,*) TRIM( ThermoState % Names(j) )
+    WRITE(*,*) ThermoState % nValues(j)
+    WRITE(*,*) ThermoState % minValues(j), ThermoState % maxValues(j)
+    WRITE(*,*) ThermoState%States(j)%Values(:)
   END DO
 
-  WRITE(*,*) State1%States(1)%Values(1)
-  WRITE(*,*) State1%States(1)%Values(2)
-  WRITE(*,*) State1%States(1)%Values(3)
-  WRITE(*,*) State1%States(2)%Values(1)
-  WRITE(*,*) State1%States(2)%Values(2)
-  WRITE(*,*) State1%States(2)%Values(3)
-  WRITE(*,*) State1%States(3)%Values(1)
-  WRITE(*,*) State1%States(3)%Values(2)
-  WRITE(*,*) State1%States(3)%Values(3)
-
-
-  Call DeAllocateThermoState( State1 )
+  Call DeAllocateThermoState( ThermoState )
 
 END PROGRAM wlThermoStateTest
