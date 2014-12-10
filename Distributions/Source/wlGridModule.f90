41f31693653767f792a72fccb2bb561b78e680c1
Ryan Landfield <ryanlandfield@gmail.com>
2014-12-10 02:10:51 +0000

    Updated wlGridModule and wlThermoStateTest, debugging in process

--- Distributions/Source/wlGridModule.f90
+++ Distributions/Source/wlGridModule.f90
@@ -1,18 +1,20 @@
 MODULE wlGridModule
 
   USE wlKindModule, ONLY: dp
+  USE wlThermoStateModule
 
   implicit none
   PRIVATE
 
   PUBLIC MakeLinearGrid
   PUBLIC MakeLogGrid
+  PUBLIC WriteGrid 
 
 CONTAINS 
 
   SUBROUTINE MakeLinearGrid( LowerBound, UpperBound, nPoints, Grid )
 
-    Integer, INTENT(in)              :: nPoints  
+    INTEGER, INTENT(in)               :: nPoints  
     REAL(dp), INTENT(in)              :: LowerBound
     REAL(dp), INTENT(in)              :: UpperBound 
    
@@ -56,5 +58,33 @@ CONTAINS
 
   END SUBROUTINE MakeLogGrid
 
+  SUBROUTINE WriteGrid( rank, griddims, Grid ) 
+                          ! Do we want one rho t ye Grid, or 3 subgrids?
+    
+
+    INTEGER                                   :: hdferr
+    INTEGER                                   :: i 
+    INTEGER, INTENT(in)                       :: rank
+    INTEGER, DIMENSION(rank), INTENT(in)      :: griddims          
+    TYPE(ThermoStateType), INTENT(in)         :: ThermoState1    
+    INTEGER(HID_T) :: file_id
+    INTEGER(HID_T) :: dset_id
+    INTEGER(HID_T) :: space_id
+    CHARACTER(LEN=10), PARAMETER  :: filename = "wltable.h5"
+    REAL(dp), ALLOCATABLE, DIMENSION(:) :: buffer
+    CALL h5open_f(hdferr)
+    CALL h5fcreate_f(filename, H5F_ACC_TRUNC_F, file, hdferr)
+    CALL h5screate_simple_f(rank, griddims, space_id, hdferr)
+
+     DO i = 1, rank
+       CALL h5dcreate_f(file, TRIM( ThermoState1 % Names(i) ), H5T_NATIVE_DOUBLE, space_id, dset_id, hdferr) 
+         !DO j = 1, griddims(i)
+         !  buffer(j) = Thermostate1 % States(j) % Values(j)
+         !END DO
+       CALL h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, Thermostate1 % States(i) % Values(:), griddims(i), hdferr)
+       CALL h5dclose_f(dset_id, hdferr)
+     END DO
+
+  END SUBROUTINE WriteGrid
 
 END MODULE wlGridModule
--- Utilities/UnitTests/ThermoState/wlThermoStateTest.f90
+++ Utilities/UnitTests/ThermoState/wlThermoStateTest.f90
@@ -15,30 +15,32 @@ PROGRAM wlThermoStateTest
   npts = (/10,31,61/)
 
 
-  Call AllocateThermoState( ThermoState, npts )
+  CALL AllocateThermoState( ThermoState, npts )
 
   ThermoState % nValues(1:3) = npts(1:3)
   ThermoState % Names(1:3) = (/'Density                         ',&
-                          'Temperature                     ',&
-                          'Electron Fraction               '/)
+                               'Temperature                     ',&
+                               'Electron Fraction               '/)
     
   ThermoState % minValues(1:3) =  (/1.0d06,0.1d00,1.0d-02/)
   ThermoState % maxValues(1:3) =  (/1.0d15,1.0d02,6.1d-01/)
 
-  CALL MakeLogGrid( ThermoState%minValues(1), ThermoState%maxValues(1),&
-         ThermoState%nValues(1), ThermoState%States(1)%Values)
-  CALL MakeLogGrid( ThermoState%minValues(2), ThermoState%maxValues(2),&
-         ThermoState%nValues(2), ThermoState%States(2)%Values)
-  CALL MakeLinearGrid( ThermoState%minValues(3), ThermoState%maxValues(3),&
-         ThermoState%nValues(3), ThermoState%States(3)%Values)
+  CALL MakeLogGrid( ThermoState % minValues(1), ThermoState % maxValues(1),&
+         ThermoState % nValues(1), ThermoState % States(1) % Values)
+  CALL MakeLogGrid( ThermoState % minValues(2), ThermoState % maxValues(2),&
+         ThermoState % nValues(2), ThermoState % States(2) % Values)
+  CALL MakeLinearGrid( ThermoState % minValues(3), ThermoState % maxValues(3),&
+         ThermoState % nValues(3), ThermoState % States(3) % Values)
 
   DO j = 1,3
     WRITE(*,*) TRIM( ThermoState % Names(j) )
     WRITE(*,*) ThermoState % nValues(j)
     WRITE(*,*) ThermoState % minValues(j), ThermoState % maxValues(j)
-    WRITE(*,*) ThermoState%States(j)%Values(:)
+    WRITE(*,*) ThermoState % States(j) % Values(:)
   END DO
 
-  Call DeAllocateThermoState( ThermoState )
+  !CALL WriteGrid( 3, npts, ThermoState )
+  
+  CALL DeAllocateThermoState( ThermoState )
 
 END PROGRAM wlThermoStateTest
