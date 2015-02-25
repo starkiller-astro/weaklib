9265dfc0b615b59797b04e081491de5dc11d6b18
Ryan Landfield <ryanlandfield@gmail.com>
2015-02-25 22:38:47 +0000

    Corrected logical syntax for wlInterpolationModule.f90, successfully compiled and tested modified wlInterpolationTest.f90
    Former-commit-id: 2c8ec893148a6593cd0c96b295c4ce1e35080027

--- Distributions/Source/wlInterpolationModule.f90
+++ Distributions/Source/wlInterpolationModule.f90
@@ -48,6 +48,8 @@ CONTAINS
     REAL(dp), DIMENSION(:), INTENT(in) :: Coordinate3
     LOGICAL, DIMENSION(3), INTENT(in)  :: LogInterp 
     REAL(dp), DIMENSION(:,:,:), INTENT(in) :: Table
+    
+    INTEGER :: i
 
     REAL(dp), DIMENSION(:), INTENT(out) :: Interpolant 
 
@@ -57,9 +59,9 @@ CONTAINS
   
     DO i = 1, SIZE(x1)  
   
-      CALL Locate( Coordinate1, x1(i), SIZE(Coordinate1), il1 ) 
-      CALL Locate( Coordinate2, x2(i), SIZE(Coordinate2), il2 )
-      CALL Locate( Coordinate3, x3(i), SIZE(Coordinate3), il3 )
+      CALL locate( Coordinate1, SIZE(Coordinate1), x1(i), il1 ) 
+      CALL locate( Coordinate2, SIZE(Coordinate2), x2(i), il2 )
+      CALL locate( Coordinate3, SIZE(Coordinate3), x3(i), il3 )
     
       p000 = Table( il1  , il2  , il3   )
       p100 = Table( il1+1, il2  , il3   )
@@ -69,34 +71,32 @@ CONTAINS
       p101 = Table( il1+1, il2  , il3+1 )
       p011 = Table( il1  , il2+1, il3+1 )
       p111 = Table( il1+1, il2+1, il3+1 )
-  
-      IF LogInterp = (/.true.,.true.,.false./) THEN
 
+      IF ( LogInterp(1) ) THEN 
       delta(1) = LOG10( x1(i) - Coordinate1(il1) ) / LOG10( Coordinate1(il1+1) - Coordinate1(il1) )
-      delta(2) = LOG10( x2(i) - Coordinate2(il2) ) / LOG10( Coordinate2(il2+1) - Coordinate2(il2) )
-      delta(3) = ( x3(i) - Coordinate3(il3) ) / ( Coordinate3(il3+1) - Coordinate3(il3) )
-    
-      ELSEIF LogInterp = (/.true.,.false.,.true./) THEN
-
-      delta(1) = LOG10( x1(i) - Coordinate1(il1) ) / LOG10( Coordinate1(il1+1) - Coordinate1(il1) )
-      delta(2) = ( x2(i) - Coordinate2(il2) ) / ( Coordinate2(il2+1) - Coordinate2(il2) )
-      delta(3) = LOG10( x3(i) - Coordinate3(il3) ) / LOG10( Coordinate3(il3+1) - Coordinate3(il3) )
-  
-      ELSEIF LogInterp = (/.false.,.true.,.true./) THEN
-
+      ELSE
       delta(1) = ( x1(i) - Coordinate1(il1) ) / ( Coordinate1(il1+1) - Coordinate1(il1) )
+      END IF
+
+      IF ( LogInterp(2) ) THEN 
       delta(2) = LOG10( x2(i) - Coordinate2(il2) ) / LOG10( Coordinate2(il2+1) - Coordinate2(il2) )
+      ELSE
+      delta(2) = ( x2(i) - Coordinate2(il2) ) / ( Coordinate2(il2+1) - Coordinate2(il2) )
+      END IF
+
+      IF ( LogInterp(3) ) THEN 
       delta(3) = LOG10( x3(i) - Coordinate3(il3) ) / LOG10( Coordinate3(il3+1) - Coordinate3(il3) )
-  
+      ELSE
+      delta(3) = ( x3(i) - Coordinate3(il3) ) / ( Coordinate3(il3+1) - Coordinate3(il3) )
       END IF
 
       Interpolant(i) &
         = 10.d0**( &
-            + (1.0_dp - delta(3)) * ( (1.0_dp - delta(1)) * (1.0_dp - delta(2)) * p000   &                
+              (1.0_dp - delta(3)) * ( (1.0_dp - delta(1)) * (1.0_dp - delta(2)) * p000   &                
                                    +            delta(1)  * (1.0_dp - delta(2)) * p100   &
                                    + ( 1.0_dp - delta(1)) *           delta(2)  * p010   &
                                    +            delta(1)  *           delta(2)  * p110 ) &
-                        delta(3)  * ( (1.0_dp - delta(1)) * (1.0_dp - delta(2)) * p001   &
+                      + delta(3)  * ( (1.0_dp - delta(1)) * (1.0_dp - delta(2)) * p001   &
                                    +            delta(1)  * (1.0_dp - delta(2)) * p101   &
                                    +  (1.0_dp - delta(1)) *           delta(2)  * p011   &
                                    +            delta(1)  *           delta(2)  * p111 ) &
--- Utilities/UnitTests/Interpolation/wlInterpolationTest.f90
+++ Utilities/UnitTests/Interpolation/wlInterpolationTest.f90
@@ -10,9 +10,9 @@ PROGRAM wlInterpolationTest
   implicit none
 
   INTEGER  :: i
-  REAL(dp) :: rho
-  REAL(dp) :: T  
-  REAL(dp) :: Ye  
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: rho
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: T
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: Ye 
   INTEGER :: RhoLocation
   INTEGER :: TLocation
   INTEGER :: YeLocation
@@ -20,25 +20,55 @@ PROGRAM wlInterpolationTest
   TYPE(EquationOfStateTableType) :: EOSTable
 
 
-  REAL(dp) :: Interpolant
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: Interpolant
+  REAL(dp), DIMENSION(:,:), ALLOCATABLE :: rand
+  REAL(dp) :: Yemin, Yemax, logTmin, logTmax, logrhomin, logrhomax
+  INTEGER :: i
+  REAL(dp) :: LogT, logrho
 
   CALL InitializeHDF( )
 
   CALL ReadEquationOfStateTableHDF( EOSTable, "SmallEquationOfStateTable.h5" )
 
-  LogInterp = (/.true.,.true.,.false./) 
+  LogInterp = (/.true.,.true.,.false./)
+
+  ALLOCATE( rho(10), T(10), Ye(10), rand(10,3), Interpolant(10) ) 
+
+  CALL RANDOM_SEED( )
+  CALL RANDOM_NUMBER( rand )
+ 
+  Yemin = EOSTable % TS % minValues(3)
+  Yemax = EOSTable % TS % maxValues(3)
+  logTmin = LOG10( EOSTable % TS % minValues(2) )
+  logTmax = LOG10( EOSTable % TS % maxValues(2) )
+  logrhomin = LOG10( EOSTable % TS % minValues(1) )
+  logrhomax = LOG10( EOSTable % TS % maxValues(1) )
+  DO i=1,10
+    Ye(i) = (Yemax - Yemin ) * rand(i,3) + Yemin   
+    logT = (logTmax - logTmin ) * rand(i,2) + logTmin   
+    T(i) = 10.d0**logT 
+    logrho = (logrhomax - logrhomin ) * rand(i,1) + logrhomin   
+    rho(i) = 10.d0**logrho 
+  END DO 
+
+  WRITE (*,*) "rho=", rho
+  WRITE (*,*) "T=", T 
+  WRITE (*,*) "Ye=", Ye 
   
-  rho = ( EOSTable % TS % States(1) % Values(6) -         &
-          EOSTable % TS % States(1) % Values(5) )/2.0d0 + &
-          EOSTable % TS % States(1) % Values(5) 
+!  rho = ( EOSTable % TS % States(1) % Values(6) -         &
+!          EOSTable % TS % States(1) % Values(5) )/2.0d0 + &
+!          EOSTable % TS % States(1) % Values(5) 
 
-  T = ( EOSTable % TS % States(2) % Values(6) -         &
-        EOSTable % TS % States(2) % Values(5) )/2.0d0 + &
-        EOSTable % TS % States(2) % Values(5) 
+!  T = ( EOSTable % TS % States(2) % Values(6) -         &
+!        EOSTable % TS % States(2) % Values(5) )/2.0d0 + &
+!        EOSTable % TS % States(2) % Values(5) 
 
-  Ye = ( EOSTable % TS % States(3) % Values(6) -         &
-         EOSTable % TS % States(3) % Values(5) )/2.0d0 + &
-         EOSTable % TS % States(3) % Values(5) 
+!  Ye = ( EOSTable % TS % States(3) % Values(6) -         &
+!         EOSTable % TS % States(3) % Values(5) )/2.0d0 + &
+!         EOSTable % TS % States(3) % Values(5) 
+
+   EOSTable % DV % Variables(1) % Values(:,:,:) &
+           = LOG10( EOSTable % DV % Variables(1) % Values(:,:,:) ) 
 
   CALL LogInterpolateSingleVariable( rho, T, Ye,                                   &
                                      EOSTable % TS % States(1) % Values,           &
@@ -47,7 +77,7 @@ PROGRAM wlInterpolationTest
                                      LogInterp,                                    &
                                      EOSTable % DV % Variables(1) % Values(:,:,:), & 
                                      Interpolant )
-   WRITE (*,*) Interpolant
+   WRITE (*,*) "Pressure =", Interpolant
 
 
   CALL DeAllocateEquationOfStateTable( EOSTable )
