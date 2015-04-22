afe3121aa68aa1b1234f676522224a660cec4b5d
Ryan Landfield <ryanlandfield@gmail.com>
2015-04-22 21:45:38 +0000

    Modifield wlIOModuleCHIMERA.f90 and wlChimeraProfileReader1DTest.f90 to read data directly from Chimera output files; compiled and tested successfully.
    Former-commit-id: 9f9a903d09c9fc4cf1685fea6853212fea6b1f5f

--- Distributions/Source/wlIOModuleCHIMERA.f90
+++ Distributions/Source/wlIOModuleCHIMERA.f90
@@ -11,39 +11,144 @@ MODULE wlIOModuleCHIMERA
 
 CONTAINS
 
-  SUBROUTINE ReadChimeraProfile1D( FileName, MaxZone, r, rho, T, Ye, s, SkipLinesOption )
+  SUBROUTINE ReadChimeraProfile1D( FileName, MaxZone, r, rho, T, Ye, p, s, &
+                                     e_internal, xn, xp, xhe, xa, chem_n,  &
+                                     chem_p, chem_e, a_heavy, z_heavy,     &
+                                     be_heavy, u, rstmss, vsound,          &
+                                     SkipLinesOption )
 
     CHARACTER(len=*), INTENT(in) :: FileName
     INTEGER, INTENT(in)          :: MaxZone
     INTEGER, INTENT(in), OPTIONAL :: SkipLinesOption
-    REAL(dp), DIMENSION(:), INTENT(out) :: r, rho, T, Ye, s
+!    REAL(dp), DIMENSION(:), INTENT(out) :: r, rho, T, Ye, s
+    REAL(dp), DIMENSION(:), INTENT(out) :: r, rho, T, Ye, p, s, e_internal, xn, &
+                                             xp, xhe, xa, chem_n, chem_p, chem_e, &
+                                             a_heavy, z_heavy, be_heavy, u, &
+                                             rstmss, vsound  
 
     INTEGER :: i
-    INTEGER :: j, FileUnit, SkipLines
+    INTEGER :: j, FileUnit, SkipLines, istate
     LOGICAL, DIMENSION(MaxZone) :: L, shock
     CHARACTER(len=1), DIMENSION(MaxZone) :: nnse, EOS
-    REAL(dp), DIMENSION(MaxZone) :: u, v, w, dr, dummy14, flat, lum, &
-                                           rstmss, Tmev, ah
+    CHARACTER(LEN=255) :: stanza_name   ! Name of stanza headers
+    REAL(dp), DIMENSION(MaxZone) :: v, w, dr, dummy14, flat, lum, &
+                                      Tmev, ah, dum1, dum2, dum3, &
+                                      dum4, dum5, dum6, dum7,     &
+                                      dum8, dum9, dum10, dum11,   &
+                                      dum12, dum13
+    !REAL(dp), DIMENSION(MaxZone) :: u, v, w, dr, dummy14, flat, lum, &
+    !                                       rstmss, Tmev, ah
 
     257 FORMAT (1x,i4,6es11.3,a1,es11.3,L3,es11.3,es14.6,4(es11.3),es10.2,a1,es11.3,2x,a1)
+    258 FORMAT (1x,i4,9es11.3)
+    259 FORMAT (1x,i4,8es11.4)
+    260 FORMAT (1x,i4,11es11.3)
+    261 FORMAT (1x,i4,14es11.3)
 
     SkipLines = 0
     IF ( Present ( SkipLinesOption ) ) &
       SkipLines = SkipLinesOption
 
-    OPEN(unit = FileUnit, file = FileName, status ="old" )
+    OPEN(unit = FileUnit, file = FileName, status ="old", iostat=istate )
 
     DO i = 1, SkipLines
       READ(FileUnit,*)
     END DO
 
-    DO i = MaxZone, 1, -1
-      READ(FileUnit, 257) j, u(i), v(i), w(i), r(i), dr(i), dummy14(i), shock(i), &
-                          flat(i), L(i), lum(i), rstmss(i), rho(i), Tmev(i),   &
-                          T(i), s(i), ah(i), nnse(i), Ye(i), EOS(i)
+    DO
+      IF ( istate /= 0) EXIT
 
+      READ(FileUnit,'(a)',iostat=istate) stanza_name
+
+      IF ( TRIM(ADJUSTL(stanza_name)) == 'Model configuration' ) THEN
+        PRINT*, TRIM(ADJUSTL(stanza_name))
+        DO i = 1, 4 
+          READ(FileUnit,*)
+        END DO
+
+        DO i = MaxZone, 1, -1
+          READ(FileUnit, 257) j, u(i), v(i), w(i), r(i), dr(i), dummy14(i), shock(i), &
+                              flat(i), L(i), lum(i), rstmss(i), rho(i), Tmev(i),   &
+                              T(i), s(i), ah(i), nnse(i), Ye(i), EOS(i)
+        END DO
+      END IF
+
+      IF ( TRIM(ADJUSTL(stanza_name))&
+           == 'Hydrodynamic data -  Pressure, energy, and derivatives' ) THEN
+        PRINT*, TRIM(ADJUSTL(stanza_name))
+        DO i = 1, 4 
+          READ(FileUnit,*)
+        END DO
+
+        DO i = MaxZone, 1, -1
+          READ(FileUnit, 258) j, dum1(i), p(i), dum2(i), dum3(i), dum4(i), &
+                              dum5(i), dum6(i), dum7(i), dum8(i)
+        END DO
+      END IF
+
+      IF ( TRIM(ADJUSTL(stanza_name)) == 'EVH1 Energy Data' ) THEN
+        PRINT*, TRIM(ADJUSTL(stanza_name))
+        DO i = 1, 4 
+          READ(FileUnit,*)
+        END DO
+
+        DO i = MaxZone, 1, -1
+          READ(FileUnit, 259) j, e_internal(i), dum1(i), dum2(i), dum3(i), dum4(i), &
+                              dum5(i), dum6(i), dum7(i)
+        END DO
+      END IF
+
+      IF ( TRIM(ADJUSTL(stanza_name)) == 'Masses and Mass averaged data' ) THEN
+        PRINT*, TRIM(ADJUSTL(stanza_name))
+        DO i = 1, 2 
+          READ(FileUnit,*)
+        END DO
+
+        READ(FileUnit,'(a)',iostat=istate) stanza_name
+        PRINT*, TRIM(ADJUSTL(stanza_name(11:13)))
+        IF ( TRIM(ADJUSTL(stanza_name(11:13))) == 'rho' ) THEN 
+          DO i = 1, 2 
+            READ(FileUnit,*)
+          END DO
+          DO i = MaxZone, 1, -1
+            READ(FileUnit, 260) j, dum1(i), dum2(i), xn(i), xp(i), xhe(i), xa(i), &
+                                   dum3(i), dum4(i), dum5(i), dum6(i), dum7(i)
+          END DO
+        END IF
+      END IF
+
+      IF ( TRIM(ADJUSTL(stanza_name)) == 'Hydrodynamic data' ) THEN
+        PRINT*, TRIM(ADJUSTL(stanza_name))
+        DO i = 1, 4 
+          READ(FileUnit,*)
+        END DO
+
+        DO i = MaxZone, 1, -1
+          READ(FileUnit, 261) j, dum1(i), dum2(i), dum3(i), dum4(i), &
+                              dum5(i), vsound(i), dum6(i), dum7(i), dum8(i), &
+                              dum9(i), dum10(i), dum11(i), dum12(i), dum13(i)
+        END DO
+      END IF
+
+    a_heavy = 0.d0
+    z_heavy = 0.d0
+    be_heavy = 0.d0
+ 
     END DO
 
+    ! Repeat this loop, for each variable if necessary, as reads with lots of dummy arguments
+    ! For each required quantity.  It may be necessary to do multiple skiplines, or rather, to 
+    ! just "GO TO LINE "  
+    ! (1) REUSE 257 w/ dummy arguments: rho T Ye and S are on line 47
+    ! (2) p is on line 12024 DONE
+    ! (3) e_internal is on line 3093 DONE
+    ! (4) xn          xp         xhe       xa are on line 7636 DONE
+    ! (5) vsound is on line 9829 DONE
+    ! (6) Chemical potentials n_cm_pot p_cm_pot e_cm_pot are on line 28958
+    ! (7) a, n, z, ahoy, zhvy, ahehvy, zhehvy, a_nphehvy, z_nphehvy are on line 26032
+    ! Create formats for each read
+
+    ! Collect independent, dependent, and new variables and put in format needed for interpolationtest
     CLOSE(FileUnit)
 
   END SUBROUTINE ReadChimeraProfile1D
--- Utilities/UnitTests/ChimeraReader1D/wlChimeraProfileReader1DTest.f90
+++ Utilities/UnitTests/ChimeraReader1D/wlChimeraProfileReader1DTest.f90
@@ -9,18 +9,44 @@ PROGRAM wlChimeraProfileReader1Dtest
   REAL(dp), DIMENSION(:), ALLOCATABLE :: T
   REAL(dp), DIMENSION(:), ALLOCATABLE :: Ye 
   REAL(dp), DIMENSION(:), ALLOCATABLE :: r 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: p 
   REAL(dp), DIMENSION(:), ALLOCATABLE :: s 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: e_internal
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: chem_n
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: chem_p
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: chem_e
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: xn 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: xp 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: xhe 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: xa 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: a_heavy 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: z_heavy 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: be_heavy 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: u 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: rstmss 
+  REAL(dp), DIMENSION(:), ALLOCATABLE :: vsound 
 
   nout = 3216
   Maxzone = 720
 
-  ALLOCATE( r( Maxzone ), rho( Maxzone ), T( Maxzone ), Ye( Maxzone ), s( Maxzone ) ) 
+  ALLOCATE( r( Maxzone ), rho( Maxzone ), T( Maxzone ), Ye( Maxzone ),     &
+              p( Maxzone ), s( Maxzone ), e_internal( Maxzone ),           &
+              xn( Maxzone ), xp( Maxzone ), xhe( Maxzone ), xa( Maxzone ), &
+              chem_n( Maxzone ), chem_p( Maxzone ), chem_e( Maxzone ),     &
+              a_heavy( Maxzone ), z_heavy( Maxzone ), be_heavy( Maxzone ), &
+              u( Maxzone ), rstmss( Maxzone ), vsound( Maxzone ) ) 
 
-  CALL ReadChimeraProfile1D( "10ms.d", Maxzone, r, rho, T, Ye, s, 46)
+  CALL ReadChimeraProfile1D( "10ms.d", Maxzone, r, rho, T, Ye, p, s, e_internal, &
+                               xn, xp, xhe, xa, chem_n, chem_p, chem_e, a_heavy,  &
+                               z_heavy, be_heavy, u, rstmss, vsound, 6932)
 
-  OPEN(nout, FILE="Output10ms10ms10ms10ms10ms10ms10ms10ms10ms10ms.d")
+  OPEN(nout, FILE="Output10ms.d")
+
   DO i = Maxzone, 1, -1
-    WRITE (nout,'(5(es12.5,x))') r(i), rho(i), T(i), Ye(i), s(i) 
+    WRITE (nout,'(20(es12.5,x))') r(i), rho(i), T(i), Ye(i), p(i), s(i), e_internal(i), &
+                                    xn(i), xp(i), xhe(i), xa(i), chem_n(i), chem_p(i),  &
+                                    chem_e(i), a_heavy(i), z_heavy(i), be_heavy(i),     &
+                                    u(i), rstmss(i), vsound(i) 
   END DO
   CLOSE(nout)
 
