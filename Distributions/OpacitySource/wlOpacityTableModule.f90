9285b0a2be0734fd62cf19eb9df93d346ebd263a
Eirik Endeve <eendeve@ornl.gov>
2016-03-31 19:22:54 +0000

    Cosmetic Changes
    Former-commit-id: f9749eacdb952c038458913af37de49c21ae15e3

--- Distributions/OpacitySource/wlOpacityTableModule.f90
+++ Distributions/OpacitySource/wlOpacityTableModule.f90
@@ -48,11 +48,14 @@ MODULE wlOpacityTableModule
 !-----------------------------------------------------------------------
  
   USE wlKindModule, ONLY: dp
-  USE HDF5
+  
   USE wlGridModule, ONLY: EnergyGridType 
   USE wlEquationOfStateTableModule
   USE wlIOModuleHDF
   USE wlEOSIOModuleHDF, ONLY: ReadEquationOfStateTableHDF
+  USE wlExtPhysicalConstantsModule, ONLY: &
+    kMeV, therm1, therm2, dmnp, me, rmu
+  USE HDF5
 
   implicit none
   PRIVATE
@@ -66,6 +69,7 @@ MODULE wlOpacityTableModule
 ! OpacityType C for  NISO( e_in, e_out, rho, T, Ye, l)
 !         
 !---------------------------------------------
+
   TYPE, PUBLIC :: OpacityTypeA  
     CHARACTER(LEN=32) :: Names
     CHARACTER(LEN=32) :: Units
@@ -89,16 +93,14 @@ MODULE wlOpacityTableModule
 !=======================================
 
   TYPE, PUBLIC :: OpacityTableType
-  
     TYPE(OpacityTypeA), ALLOCATABLE, DIMENSION(:) :: ECAPEM 
     TYPE(EnergyGridType)                          :: EnergyGrid
-    TYPE(EquationOfStateTableType)       :: EOSTable           
+    TYPE(EquationOfStateTableType)                :: EOSTable
   END TYPE 
 
- 
-   PUBLIC AllocateOpacityTable 
-   PUBLIC DeAllocateOpacityTable
-   PUBLIC totalECapEm
+  PUBLIC AllocateOpacityTable 
+  PUBLIC DeAllocateOpacityTable
+  PUBLIC totalECapEm
 
 CONTAINS
 
@@ -106,28 +108,26 @@ CONTAINS
 !==========================================================================
 ! Public Subroutine for OpacityTable
 !==========================================================================
- SUBROUTINE AllocateOpacityTable( OpacityTable, nSpeciesA, nPointsE ) ! 'EOSTable' refers to the EOSTable we want to connect
 
-     TYPE(OpacityTableType), INTENT(inout)  :: OpacityTable
-     INTEGER, INTENT(in)                    :: nPointsE
-     INTEGER, INTENT(in)                    :: nSpeciesA
+  SUBROUTINE AllocateOpacityTable( OpacityTable, nSpeciesA, nPointsE )
 
-  WRITE(*,*) "Readin EOSTable" ! make the readin EOSTable in readOpacityTable surbroutine later on
+    TYPE(OpacityTableType), INTENT(inout)  :: OpacityTable
+    INTEGER, INTENT(in)                    :: nPointsE
+    INTEGER, INTENT(in)                    :: nSpeciesA
 
-   CALL InitializeHDF( )
-   CALL ReadEquationOfStateTableHDF( OpacityTable % EOSTable, &
-                                  & "EquationOfStateTable.h5" )
+    CALL InitializeHDF( )
+    CALL ReadEquationOfStateTableHDF &
+           ( OpacityTable % EOSTable, "EquationOfStateTable.h5" )
 
-  WRITE(*,*) 'Allocating OpacityTypeA' 
-     CALL AllocateOpacityTypeA &
-          ( OpacityTable % ECAPEM, nSpeciesA, &
-           &OpacityTable % EOSTable % nPoints, nPointsE )  ! just for TypeA
-  WRITE(*,*) 'Allocating EnergyGrid' 
-     CALL AllocateEnergyGrid &
-          ( OpacityTable % EnergyGrid, nPointsE)
+    WRITE(*,*) 'Allocating OpacityTypeA' 
+    CALL AllocateOpacityTypeA &
+           ( OpacityTable % ECAPEM, nSpeciesA, &
+             OpacityTable % EOSTable % nPoints, nPointsE )  ! just for TypeA
 
-  WRITE(*,*) 'Allocation Complete'
+    WRITE(*,*) 'Allocating EnergyGrid' 
+    CALL AllocateEnergyGrid( OpacityTable % EnergyGrid, nPointsE )
 
+    WRITE(*,*) 'Allocation Complete'
    
   END SUBROUTINE AllocateOpacityTable
 
@@ -136,8 +136,8 @@ CONTAINS
 
     TYPE(OpacityTableType) :: OpacityTable
 
-    CALL DeAllocateOpacityTypeA( OpacityTable % ECAPEM)
-    CALL DeAllocateEquationOfStateTable( OpacityTable % EOSTable) 
+    CALL DeAllocateOpacityTypeA( OpacityTable % ECAPEM )
+    CALL DeAllocateEquationOfStateTable( OpacityTable % EOSTable )
     CALL DeAllocateEnergyGrid( OpacityTable % EnergyGrid ) 
  
   END SUBROUTINE DeAllocateOpacityTable
@@ -146,6 +146,7 @@ CONTAINS
 !==========================================================================
 ! subroutine for OpacityTable
 !==========================================================================
+
   SUBROUTINE AllocateOpacityTypeA( ECAPEM, nSpeciesA, nPointsTS, nPointsE )  ! For OpacityTypeA (D ='4')
 
     TYPE(OpacityTypeA), ALLOCATABLE, DIMENSION(:), INTENT(inout) :: ECAPEM
@@ -168,12 +169,12 @@ CONTAINS
 
   SUBROUTINE DeAllocateOpacityTypeA( ECAPEM ) 
 
-     TYPE(OpacityTypeA), DIMENSION(:), INTENT(inout) :: ECAPEM 
+    TYPE(OpacityTypeA), DIMENSION(:), INTENT(inout) :: ECAPEM 
  
     INTEGER :: i
   
     DO i = 1, SIZE ( ECAPEM )
-       DEALLOCATE( ECAPEM(i) % Values )
+      DEALLOCATE( ECAPEM(i) % Values )
     END DO
 
   END SUBROUTINE DeAllocateOpacityTypeA
@@ -181,10 +182,11 @@ CONTAINS
 !==========================================================================
 ! subroutine for EnergyGrid
 !==========================================================================
+
   SUBROUTINE AllocateEnergyGrid( EnergyGrid, nPointsE )
 
-    TYPE(EnergyGridType), INTENT(inout)      :: EnergyGrid
-    INTEGER, INTENT(in)                         :: nPointsE
+    TYPE(EnergyGridType), INTENT(inout) :: EnergyGrid
+    INTEGER, INTENT(in)                 :: nPointsE
 
     EnergyGrid % nPointsE = nPointsE 
 
@@ -196,48 +198,48 @@ CONTAINS
 
   SUBROUTINE DeAllocateEnergyGrid( EnergyGrid )
     
-    TYPE(EnergyGridType)            :: EnergyGrid
+    TYPE(EnergyGridType) :: EnergyGrid
  
     DEALLOCATE( EnergyGrid % Values )  
 
   END SUBROUTINE DeAllocateEnergyGrid
- 
-
 
 !========================Function=============================
-REAL FUNCTION totalECapEm(energy, rho, T, Z, A, chem_e, chem_n, chem_p, xheavy, xn, xp )
 
-    USE wlKindModule, ONLY: dp
-    USE wlExtPhysicalConstantsModule ! kmev, therm1, therm2, dmnp, me, rmu (mean baryon mass [g])
-  
-	REAL(dp) :: energy, rho, T, Z, A, chem_e, chem_n, chem_p, xheavy, xn, xp   
-        REAL(dp) :: TMeV, n, qpri, nhn, npz, etapn, jnucleon, jnuclear
-        TMeV = T * kmev                      ! kmev = 8.61733d-11 [MeV K^{-1}]
-        n = a - z
-        qpri = chem_n - chem_p + 3.0         ! [MeV] 3MeV: energy of the 1f5/2 level
-        
- 
-        if(n.le.34.0) nhn= 6.0
-        if(n.gt.34.0.and.n.le.40.0) nhn= 40.0 - n
-        if(n.gt.40.0) nhn= 0.0
+  PURE REAL(dp) FUNCTION &
+    totalECapEm( energy, rho, T, Z, A, chem_e, chem_n, chem_p, xheavy, xn, xp )
 
-        if(z.le.20.0) npz= 0.0
-        if(z.gt.20.0.and.z.le.28.0) npz= z - 20.0
-        if(z.gt.28.0) npz= 8.0
+    REAL(dp), INTENT(in) :: energy, rho, T, Z, A, chem_e, chem_n, chem_p, &
+                            xheavy, xn, xp
 
-        etapn = rho * ( xn - xp ) / ( rmu * ( EXP( (chem_n-chem_p)/TMeV ) - 1 ) ) 
+    REAL(dp) :: TMeV, n, qpri, nhn, npz, etapn, jnucleon, jnuclear
 
-        jnucleon = therm1 * etapn * (energy+dmnp)**2 &
-                   * SQRT( 1.0 - ( me / (energy+dmnp) )**2 ) &
-                   / ( EXP( (energy+dmnp-chem_e) / TMeV ) + 1 )
-   
-        jnuclear = therm2 * rho * xheavy * npz * nhn * (energy+qpri)**2&
-                   * SQRT( 1.0 - ( me / (energy+qpri) )**2 ) &
-                   / ( rmu * a * ( EXP( (energy+qpri-chem_e) / TMeV ) + 1 ))
+    TMeV = T * kmev                 ! kmev = 8.61733d-11 [MeV K^{-1}]
+    N    = A - Z
+    qpri = chem_n - chem_p + 3.0_dp ! [MeV] 3MeV: energy of the 1f5/2 level
 
-        totalECapEm = jnucleon + jnuclear
+    if(n.le.34.0)               nhn = 6.0_dp
+    if(n.gt.34.0.and.n.le.40.0) nhn = 40.0_dp - N
+    if(n.gt.40.0)               nhn = 0.0_dp
+
+    if(z.le.20.0)               npz = 0.0
+    if(z.gt.20.0.and.z.le.28.0) npz = z - 20.0
+    if(z.gt.28.0)               npz = 8.0
+
+    etapn = rho * ( xn - xp ) / ( rmu * ( EXP( (chem_n-chem_p)/TMeV ) - 1 ) ) 
+
+    jnucleon = therm1 * etapn * (energy+dmnp)**2 &
+                 * SQRT( 1.0_dp - ( me / (energy+dmnp) )**2 ) &
+                 / ( EXP( (energy+dmnp-chem_e) / TMeV ) + 1.0_dp )
+
+    jnuclear = therm2 * rho * xheavy * npz * nhn * (energy+qpri)**2 &
+                 * SQRT( 1.0_dp - ( me / (energy+qpri) )**2 ) &
+                 / ( rmu * a * ( EXP( (energy+qpri-chem_e) / TMeV ) + 1_dp ) )
+
+    totalECapEm = jnucleon + jnuclear
 
     RETURN 
-END FUNCTION
+  END FUNCTION totalECapEm
+
 
 END MODULE wlOpacityTableModule
