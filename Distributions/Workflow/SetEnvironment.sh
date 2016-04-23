9444bb83336c9af9adb07dc2a667537f86bde33a
Eirik Endeve <eendeve@ornl.gov>
2016-04-23 20:16:32 +0000

    Added gnu compiler flags for titan
    Former-commit-id: 85d6284d902cabc5b43338abc38b13448c1a29f5

---
+++ Distributions/Workflow/SetEnvironment.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export WEAKLIB_MACHINE=$1
+
+if [[ $WEAKLIB_MACHINE == titan* ]]; then
+
+  echo
+  echo "INFO: Setting environment for" $WEAKLIB_MACHINE
+
+  source ${MODULESHOME}/init/bash
+
+  module unload fftw cray-hdf5 cray-petsc silo subversion
+  module unload pgi gcc cce pathscale
+  module unload PrgEnv-pgi PrgEnv-gnu PrgEnv-cray PrgEnv-pathscale
+
+elif [[ $WEAKLIB_MACHINE == sn1987b* ]]; then
+
+  echo
+  echo "INFO: Setting environment for" $WEAKLIB_MACHINE
+
+fi
+
+
+if [[ $WEAKLIB_MACHINE == titan_gnu ]]; then
+
+  echo
+
+  module load PrgEnv-gnu
+  module load cray-hdf5
+
+elif [[ $WEAKLIB_MACHINE == sn1987b ]]; then
+
+  echo
+
+else
+
+  echo "  WARNING: Unknown machine " $WEAKLIB_MACHINE
+
+fi
