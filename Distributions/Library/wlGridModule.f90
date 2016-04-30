605e9d73e5a298a2efc021c18e31d410c3e8aa80
Eirik Endeve <eendeve@ornl.gov>
2016-04-30 11:14:46 +0000

    Removed EnergyGridType from wlGridModule.  Streamlining of MakeLinearGrid.
    Former-commit-id: 119a71e076c75909aa2f7820b4a9991ff7ead7c9

--- Distributions/Library/wlGridModule.f90
+++ Distributions/Library/wlGridModule.f90
@@ -8,46 +8,31 @@ MODULE wlGridModule
   PUBLIC MakeLinearGrid
   PUBLIC MakeLogGrid
 
-  TYPE, PUBLIC :: EnergyGridType
-    CHARACTER(LEN=32) :: Names='Energy'
-    CHARACTER(LEN=32) :: Units='MeV'
-    INTEGER  :: nPointsE
-    REAL(dp) :: minValue
-    REAL(dp) :: maxValue
-!    REAL(dp) :: minWidth
-!    REAL(dp) :: alpha  ! constant in geometric series
-    REAL(dp), ALLOCATABLE, DIMENSION(:) :: Values
-  END TYPE
-
-
 CONTAINS 
+
 
   SUBROUTINE MakeLinearGrid( LowerBound, UpperBound, nPoints, Grid )
 
-    INTEGER, INTENT(in)               :: nPoints  
-    REAL(dp), INTENT(in)              :: LowerBound
-    REAL(dp), INTENT(in)              :: UpperBound 
-   
-    REAl(dp), DIMENSION(nPoints), INTENT(out)   :: Grid 
+    INTEGER,                      INTENT(in)  :: nPoints
+    REAL(dp),                     INTENT(in)  :: LowerBound
+    REAL(dp),                     INTENT(in)  :: UpperBound
+    REAl(dp), DIMENSION(nPoints), INTENT(out) :: Grid 
 
-    Integer  :: m
+    INTEGER  :: i
     REAL(dp) :: BinWidth
 
     BinWidth = ( UpperBound - LowerBound ) / DBLE( nPoints - 1 )
 
-    Grid(1) = LowerBound
- 
-    DO m = 2, nPoints - 1
-      Grid(m) = Grid(m-1) + BinWidth
+    DO i = 1, nPoints
+      Grid(i) = LowerBound + DBLE( i - 1 ) * BinWidth
     END DO
-
-    Grid(nPoints) = UpperBound
-
+ 
   END SUBROUTINE MakeLinearGrid
+
 
   SUBROUTINE MakeLogGrid( LowerBound, UpperBound, nPoints, Grid )
 
-    Integer, INTENT(in)      :: nPoints          
+    Integer, INTENT(in)      :: nPoints
     REAL(dp), INTENT(in)     :: LowerBound
     Real(dp), INTENT(in)     :: UpperBound
 
@@ -67,5 +52,6 @@ CONTAINS
     Grid(nPoints) = UpperBound
 
   END SUBROUTINE MakeLogGrid
+
 
 END MODULE wlGridModule
