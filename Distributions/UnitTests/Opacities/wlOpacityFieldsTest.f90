f88c128802348c39749a409dd953a241a3dfb1bd
Eirik Endeve <eendeve@ornl.gov>
2016-04-30 22:59:44 +0000

    Full draft of wlOpacityFieldsTest
    Former-commit-id: 3b06240340d0709f3ed7420013683a2c46597bf4

--- Distributions/UnitTests/Opacities/wlOpacityFieldsTest.f90
+++ Distributions/UnitTests/Opacities/wlOpacityFieldsTest.f90
@@ -14,7 +14,7 @@ PROGRAM wlOpacityFieldsTest
 
   IMPLICIT NONE
 
-  INTEGER :: iE, iD, iT, iY
+  INTEGER :: iE, iEp, iD, iT, iY, il
   TYPE(OpacityTableType) :: OpacityTable
 
   CALL AllocateOpacityTable &
@@ -33,6 +33,8 @@ PROGRAM wlOpacityFieldsTest
 
   EnergyGrid % MinValue = 3.0d-1
   EnergyGrid % MaxValue = 3.0d+2
+
+  EnergyGrid % LogInterp = 1
 
   CALL MakeLogGrid &
          ( EnergyGrid % MinValue, EnergyGrid % MaxValue, &
@@ -80,31 +82,72 @@ PROGRAM wlOpacityFieldsTest
 
   END ASSOCIATE ! ecap
 
+  ! -- Inelastic Scattering --
 
   ASSOCIATE( scatt_Iso => OpacityTable % scatt_Iso )
 
-    scatt_Iso % Names &
-      = [ 'Elastic Scattering on Nuclei  ' ]
+  scatt_Iso % Names &
+    = [ 'Elastic Scattering on Nuclei  ' ]
 
-    scatt_Iso % Species &
-      = [ 'Electron Neutrino             ' ]
+  scatt_Iso % Species &
+    = [ 'Electron Neutrino             ' ]
 
-    scatt_Iso % Units &
-      = [ 'Per Centimeter                ' ]
+  scatt_Iso % Units &
+    = [ 'Per Centimeter                ' ]
+
+
+  ASSOCIATE( Sig_Nu_e => scatt_Iso % Kernel(iNu_e) % Values )
+
+  DO il = 1, scatt_Iso % nMoments
+    DO iY = 1, scatt_Iso % nPoints(4)
+      DO iT = 1, scatt_Iso % nPoints(3)
+        DO iD = 1, scatt_Iso % nPoints(2)
+          DO iE = 1, scatt_Iso % nPoints(1)
+
+            Sig_Nu_e(iE,iD,iT,iY,il) = 2.0_dp
+    
+          END DO
+        END DO
+      END DO
+    END DO
+  END DO
+
+  END ASSOCIATE ! Sig_Nu_e
 
   END ASSOCIATE ! scatt_Iso
 
+  ! -- Elastic Scattering --
 
   ASSOCIATE( scatt_nIso => OpacityTable % scatt_nIso )
 
-    scatt_nIso % Names &
-      = [ 'Inelastic Scattering on Elect.' ]
+  scatt_nIso % Names &
+    = [ 'Inelastic Scattering on Elect.' ]
 
-    scatt_nIso % Species &
-      = [ 'Electron Neutrino             ' ]
+  scatt_nIso % Species &
+    = [ 'Electron Neutrino             ' ]
 
-    scatt_nIso % Units &
-      = [ 'Per Centimeter Per MeV**3     ' ]
+  scatt_nIso % Units &
+    = [ 'Per Centimeter Per MeV**3     ' ]
+
+  ASSOCIATE( Sig_Nu_e => scatt_nIso % Kernel(iNu_e) % Values )
+
+  DO il = 1, scatt_nIso % nMoments
+    DO iY = 1, scatt_nIso % nPoints(4)
+      DO iT = 1, scatt_nIso % nPoints(3)
+        DO iD = 1, scatt_nIso % nPoints(2)
+          DO iEp = 1, scatt_nIso % nPoints(1)
+            DO iE = 1, scatt_nIso % nPoints(1)
+
+              Sig_Nu_e(iE,iEp,iD,iT,iY,il) = 3.0_dp
+
+            END DO
+          END DO
+        END DO
+      END DO
+    END DO
+  END DO
+
+  END ASSOCIATE ! Sig_Nu_e
 
   END ASSOCIATE ! scatt_nIso
 
