d6064caf7b13a355512135f99cd7ca7ca99159d4
ranchu1 <rchu@vols.utk.edu>
2017-06-12 05:02:47 +0000

    Added new function to determine whether the number distribution function satisfy the equilibrium requirement.
    Former-commit-id: fa57ca4f9f2efc113ed4396d49555fa7d052eab8
    Former-commit-id: 7d9d2027addf3cd4f4c11b33255a1b9d5fb5e814

---
+++ External/Math/MatlabCode/isNeq.m
@@ -0,0 +1,21 @@
+function [ output_args ] = isNeq( N, R_In_H, R_Out_H, theta, N_g )
+%Test whether the distribution function N is
+%   the equilibrium number distribution function
+
+output_args = false;
+
+LN = L_FUN( N, R_In_H, R_Out_H, theta, N_g ) * N;
+
+critical = ones( N_g, 1) * 1E-10;
+
+logi =  ( abs(LN) < critical );
+
+if ( isequal( logi, ones( N_g, 1) ) )
+    output_args = true;
+    disp( fprintf( 'Is N the equilibrium number density: YES' ) );
+else
+    disp( fprintf( 'Is N the equilibrium number density: NO!' ) );
+end
+
+end
+
