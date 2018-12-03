85e82b6feccb8b62ed5dc51a3d6b1043c84fbbff
ranchu1 <rchu@vols.utk.edu>
2018-12-03 16:49:52 +0000

    Cosmetic changes after check.
    Former-commit-id: 6216618ea41a0df47e797e8dd9e380cf07527af0
    Former-commit-id: 5bbb2e786e45ce7339078d610769d9b61857c14e

--- External/Utilities/Opacities/Bruenn85/Chimera/abemrgn_weaklib.f90
+++ External/Utilities/Opacities/Bruenn85/Chimera/abemrgn_weaklib.f90
@@ -1,6 +1,7 @@
-SUBROUTINE abemrgn_weaklib( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmpp, &
-& cmpe, absor, emit, ye, nez )
-!-----------------------------------------------------------------------
+SUBROUTINE abemrgn_weaklib &
+       ( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmpp, &
+         cmpe, absor, emit, ye, nez )
+!--------------------------------------------------------------------
 !    Author:       R. Chu, Dept. Phys. & Astronomy
 !                  U. Tennesee, Knoxville
 !
@@ -13,13 +14,14 @@ SUBROUTINE abemrgn_weaklib( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmp
 !       by subroutine abemrate.
 !
 !    Subprograms called:
-!  abem_cal             : computes neutrino emission and absorption on free
-!                          neutrons and protons
-!  abemnc               : computes neutrino emission and absorption on heavy
-!                          nuclei using the FFN formalizm
+!  abem_cal      : computes neutrino emission and absorption 
+!                  on free neutrons and protons
+!  abemnc        : computes neutrino emission and absorption 
+!                  on heavy nuclei using the FFN formalizm
 !
 !    Input arguments:
-!  n             : neutrino type (1, e-neutrino; 2, e-antineutrino; 3, t-neutrino)
+!  n             : neutrino type 
+!                  (1, e-neutrino; 2, e-antineutrino; 3, t-neutrino)
 !  e_in          : neutrino energy [MeV]
 !  rho           : matter density [g cm^{-3}]
 !  t             : matter temperature [K]
@@ -28,74 +30,86 @@ SUBROUTINE abemrgn_weaklib( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmp
 !  xh            : heavy nucleus mass fraction
 !  ah            : heavy nucleus mass number
 !  zh            : heavy nucleus charge number
-!  cmpn          : free neutron chemical potential (excluding rest mass) [MeV]
-!  cmpp          : free proton chemical potential (excluding rest mass) [MeV]
-!  cmpe          : electron chemical potential (including rest mass) [MeV]
+!  cmpn          : free neutron chemical potential 
+!                  (excluding rest mass) [MeV]
+!  cmpp          : free proton chemical potential 
+!                  (excluding rest mass) [MeV]
+!  cmpe          : electron chemical potential 
+!                  (including rest mass) [MeV]
 !  ye_cube       : electron fraction
 !
 !    Output arguments:
-!  absor       : absorption inverse mean free path (/cm)
-!  emit        : emission inverse mean free path (/cm)! 
-!-----------------------------------------------------------------------
+!  absor         : absorption inverse mean free path (/cm)
+!  emit          : emission inverse mean free path (/cm)! 
+!--------------------------------------------------------------------
 
-USE kind_module, ONLY: double
-USE numerical_module, ONLY: zero, one, epsilon
-USE physcnst_module, ONLY: kmev, dmnp
-
-USE prb_cntl_module, ONLY: iaence
+USE kind_module, ONLY: &
+      double
+USE numerical_module, ONLY: &
+      zero, one, epsilon
+USE physcnst_module, ONLY: &
+      kmev, dmnp
+USE prb_cntl_module, ONLY: &
+      iaence
 
 IMPLICIT NONE
 
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 !        Input variables.
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 
-INTEGER, INTENT(in)          :: n             ! neutrino flavor index
-INTEGER, INTENT(in)          :: nez           ! number of energy groups
+INTEGER, INTENT(in)          :: n       ! neutrino flavor index
+INTEGER, INTENT(in)          :: nez     ! number of energy groups
 
-REAL(double), DIMENSION(nez), INTENT(in) :: e_in ! zone centered incoming neutrino energy [MeV]
-REAL(double), INTENT(in)    :: rho           ! density (g/cm^3)
-REAL(double), INTENT(in)    :: t             ! temperature [K]
-REAL(double), INTENT(in)    :: ye            ! electron fraction
-REAL(double), INTENT(in)    :: xneut         ! free neutron mass fraction
-REAL(double), INTENT(in)    :: xprot         ! free proton mass fraction
-REAL(double), INTENT(in)    :: xh            ! heavy nuclei mass fraction
-REAL(double), INTENT(in)    :: ah            ! heavy nuclei mass number
-REAL(double), INTENT(in)    :: zh            ! heavy nuclei charge number
-REAL(double), INTENT(in)    :: cmpn          ! neutron chemical porential
-REAL(double), INTENT(in)    :: cmpp          ! proton chemical porential
-REAL(double), INTENT(in)    :: cmpe          ! electron chemical porential
+REAL(double), DIMENSION(nez), INTENT(in) :: e_in 
+                      ! zone centered incoming neutrino energy [MeV]
+REAL(double), INTENT(in)    :: rho      ! density (g/cm^3)
+REAL(double), INTENT(in)    :: t        ! temperature [K]
+REAL(double), INTENT(in)    :: ye       ! electron fraction
+REAL(double), INTENT(in)    :: xneut    ! free neutron mass fraction
+REAL(double), INTENT(in)    :: xprot    ! free proton mass fraction
+REAL(double), INTENT(in)    :: xh       ! heavy nuclei mass fraction
+REAL(double), INTENT(in)    :: ah       ! heavy nuclei mass number
+REAL(double), INTENT(in)    :: zh       ! heavy nuclei charge number
+REAL(double), INTENT(in)    :: cmpn     ! neutron chemical porential
+REAL(double), INTENT(in)    :: cmpp     ! proton chemical porential
+REAL(double), INTENT(in)    :: cmpe     ! electron chemical porential
 
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 !        Output variables.
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 
-REAL(double), DIMENSION(nez), INTENT(out) :: absor ! inverse mean free path for absorption
-REAL(double), DIMENSION(nez), INTENT(out) :: emit  ! inverse mean free path for emission 
+REAL(double), DIMENSION(nez), INTENT(out) :: absor 
+                      ! inverse mean free path for absorption
+REAL(double), DIMENSION(nez), INTENT(out) :: emit 
+                      ! inverse mean free path for emission 
 
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 !        Local variables
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 REAL(double), DIMENSION(nez)  :: absrnp, absrnc, emisnp, emisnc
 
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 !  n-neutrino - free nucleon absorption and emission inverse mean
 !   free paths (/cm)
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 
-  CALL abem_cal_weaklib( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmpp, &
-&   cmpe, absrnp, emisnp, ye, nez )
+  CALL abem_cal_weaklib &
+        ( n, e_in, rho, t, xneut, xprot, xh, ah, zh, cmpn, cmpp, &
+          cmpe, absrnp, emisnp, ye, nez )
 
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 !  n-neutrino - nuclei absorption and emission inverse mean free
 !   paths (/cm).
-!-----------------------------------------------------------------------
+!--------------------------------------------------------------------
 
-  CALL abemnc_weaklib( n, nez, e_in, rho, t, xh, ah, zh, cmpn, cmpp,  &
-&   cmpe, absrnc, emisnc )
+  CALL abemnc_weaklib &
+        ( n, nez, e_in, rho, t, xh, ah, zh, cmpn, cmpp,  &
+          cmpe, absrnc, emisnc )
 
   absor = absrnp + absrnc
   emit  = emisnp + emisnc
 
 RETURN
+
 END SUBROUTINE abemrgn_weaklib
