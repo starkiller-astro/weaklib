c4d906ce36ceab6f8d3ab96c6585517e3da2d194
Eirik Endeve <eendeve@ornl.gov>
2019-04-18 16:02:24 +0000

    Updates to test reflecting recent weaklib changes and added testing of additional interpolation routines
    Former-commit-id: 9c7a058954f9099780d936dc582209df6bcdd219
    Former-commit-id: 3df5c4d2e39167655a6a1704bff5d9bbb20dfbfe

--- Distributions/UnitTests/EquationOfState/wlEosPerformanceTest.f90
+++ Distributions/UnitTests/EquationOfState/wlEosPerformanceTest.f90
@@ -11,18 +11,20 @@ PROGRAM wlEosPerformanceTest
     ReadEquationOfStateTableHDF
   USE wlInterpolationModule, ONLY: &
     LogInterpolateSingleVariable, &
+    LogInterpolateDifferentiateSingleVariable
+  USE wlEOSInterpolationModule, ONLY: &
     LogInterpolateAllVariables
 
   IMPLICIT NONE
 
-INTEGER :: &
+  INTEGER :: &
     n_rndm, &
-    iPoint, iV
+    iPoint, iV, iDim
   INTEGER, DIMENSION(3) :: &
     LogInterp = [ 1, 1, 0 ]
   INTEGER, PARAMETER :: &
     iD = 1, iT = 2, iY = 3, &
-    nPoints = 2**16, &
+    nPoints = 2**18, &
     nVariables = 15
   REAL(dp) :: &
     tBegin, &
@@ -33,7 +35,14 @@ INTEGER :: &
     rndm_T, &
     rndm_Y, &
     Interpolant1, &
-    Interpolant2
+    Interpolant2, &
+    Interpolant3, &
+    Error
+  REAL(dp), DIMENSION(nPoints,3) :: &
+    Derivative1, &
+    Derivative2, &
+    Derivative3, &
+    ErrorD
   REAL(dp), DIMENSION(nPoints,nVariables) :: &
     Interpolants1
   REAL(dp), DIMENSION(nVariables,nPoints) :: &
@@ -113,7 +122,7 @@ INTEGER :: &
   CALL CPU_TIME( tEnd )
 
   WRITE(*,*)
-  WRITE(*,'(A4,A40,ES10.4E2)') &
+  WRITE(*,'(A4,A48,ES10.4E2)') &
     '', 'LogInterpolateSingleVariable_3D: ', tEnd - tBegin
   WRITE(*,*)
 
@@ -125,17 +134,104 @@ INTEGER :: &
   CALL CPU_TIME( tEnd )
 
   WRITE(*,*)
-  WRITE(*,'(A4,A40,ES10.4E2)') &
+  WRITE(*,'(A4,A48,ES10.4E2)') &
     '', 'LogInterpolateSingleVariable_3D_Custom: ', tEnd - tBegin
   WRITE(*,*)
+
+  CALL CPU_TIME( tBegin )
+
+  DO iPoint = 1, nPoints
+
+    CALL LogInterpolateSingleVariable &
+           ( D(iPoint), T(iPoint), Y(iPoint), Dtab, Ttab, Ytab, OS, Ptab, &
+             Interpolant3(iPoint) )
+
+  END DO
+
+  CALL CPU_TIME( tEnd )
+
+  WRITE(*,*)
+  WRITE(*,'(A4,A48,ES10.4E2)') &
+    '', 'LogInterpolateSingleVariable_3D_Custom_Point: ', tEnd - tBegin
+  WRITE(*,*)
+
+  Error = ABS( ( Interpolant1 - Interpolant2 ) / Interpolant1 )
+
+  WRITE(*,*) MAXVAL( Error ), MINVAL( Error )
+
+  Error = ABS( ( Interpolant1 - Interpolant3 ) / Interpolant1 )
+
+  WRITE(*,*) MAXVAL( Error ), MINVAL( Error )
+
+  ! --- Single Variable (Interpolate and Differentiate) ---
+
+  CALL CPU_TIME( tBegin )
+
+  CALL LogInterpolateDifferentiateSingleVariable &
+         ( D, T, Y, Dtab, Ttab, Ytab, LogInterp, OS, Ptab, Interpolant1, &
+           Derivative1 )
+
+  CALL CPU_TIME( tEnd )
+
+  WRITE(*,*)
+  WRITE(*,'(A4,A64,ES10.4E2)') &
+    '', 'LogInterpolateDifferentiateSingleVariable_3D: ', &
+    tEnd - tBegin
+  WRITE(*,*)
+
+  CALL CPU_TIME( tBegin )
+
+  CALL LogInterpolateDifferentiateSingleVariable &
+         ( D, T, Y, Dtab, Ttab, Ytab, OS, Ptab, Interpolant2, &
+           Derivative2 )
+
+  CALL CPU_TIME( tEnd )
+
+  WRITE(*,*)
+  WRITE(*,'(A4,A64,ES10.4E2)') &
+    '', 'LogInterpolateDifferentiateSingleVariable_3D_Custom: ', &
+    tEnd - tBegin
+  WRITE(*,*)
+
+  CALL CPU_TIME( tBegin )
+
+  DO iPoint = 1, nPoints
+
+    CALL LogInterpolateDifferentiateSingleVariable &
+           ( D(iPoint), T(iPoint), Y(iPoint), Dtab, Ttab, Ytab, OS, Ptab, &
+             Interpolant3(iPoint), Derivative3(iPoint,:) )
+
+  END DO
+
+  CALL CPU_TIME( tEnd )
+
+  WRITE(*,*)
+  WRITE(*,'(A4,A64,ES10.4E2)') &
+    '', 'LogInterpolateDifferentiateSingleVariable_3D_Custom_Point: ', &
+    tEnd - tBegin
+  WRITE(*,*)
+
+  Error = ABS( Interpolant1 - Interpolant2 ) / ABS( Interpolant1 )
+  WRITE(*,*) MAXVAL( Error ), MINVAL( Error )
+  DO iDim = 1, 3
+    ErrorD(:,iDim) &
+      = ABS( ( Derivative1(:,iDim) - Derivative2(:,iDim) ) ) &
+          / MAX( ABS( Derivative1(:,iDim) ), SQRT( TINY( 1.0_dp ) ) )
+    WRITE(*,*) '  ', MAXVAL( ErrorD(:,iDim) ), MINVAL( ErrorD(:,iDim) )
+  END DO
+
+  Error = ABS( Interpolant1 - Interpolant3 ) / ABS( Interpolant1 )
+  WRITE(*,*) MAXVAL( Error ), MINVAL( Error )
+  DO iDim = 1, 3
+    ErrorD(:,iDim) &
+      = ABS( ( Derivative1(:,iDim) - Derivative3(:,iDim) ) ) &
+          / MAX( ABS( Derivative1(:,iDim) ), SQRT( TINY( 1.0_dp ) ) )
+    WRITE(*,*) '  ', MAXVAL( ErrorD(:,iDim) ), MINVAL( ErrorD(:,iDim) )    
+  END DO
 
   END ASSOCIATE ! Dtab, etc.
 
   END ASSOCIATE ! iDtab, etc.
-
-  WRITE(*,*) &
-    MAXVAL( ABS( Interpolant1 - Interpolant2 ) / Interpolant1 ), &
-    MINVAL( ABS( Interpolant1 - Interpolant2 ) / Interpolant1 )
 
   ! --- All Variables ---
 
@@ -178,11 +274,9 @@ INTEGER :: &
   WRITE(*,*)
 
   DO iV = 1, nVariables
-    WRITE(*,*) iV, &
-    MAXVAL( ABS( ( Interpolants1(:,iV) - Interpolants2(iV,:) ) &
-                 / Interpolants1(:,iV) ) ), &
-    MINVAL( ABS( ( Interpolants1(:,iV) - Interpolants2(iV,:) ) &
-                 / Interpolants1(:,iV) ) )
+    Error = ABS( Interpolants1(:,iV) - Interpolants2(iV,:) ) &
+              / ABS( Interpolants1(:,iV) )
+    WRITE(*,*) iV, EosTab % DV % Names(iV), MAXVAL( Error ), MINVAL( Error )
   END DO
   WRITE(*,*)
 
