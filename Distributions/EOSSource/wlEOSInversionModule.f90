8ceee768f80dd5d9056b478a6bd6ad1621c409db
Austin Harris <jaharris87@gmail.com>
2019-05-10 15:37:33 +0000

    Rework temperature inversion routine (ComputeTemperatureWith_DEY) to run well on GPU
    Former-commit-id: 450e92c60a9c6656f9efd4a6faecf240cc254f63
    Former-commit-id: 57539d8db3f6b47d9f163cae9e4a32a6291be6e7

--- Distributions/EOSSource/wlEOSInversionModule.f90
+++ Distributions/EOSSource/wlEOSInversionModule.f90
@@ -35,7 +35,8 @@ MODULE wlEOSInversionModule
 
   INTERFACE ComputeTemperatureWith_DEY
     MODULE PROCEDURE ComputeTemperatureWith_DEY_Many
-    MODULE PROCEDURE ComputeTemperatureWith_DEY_Single
+    MODULE PROCEDURE ComputeTemperatureWith_DEY_Single_Guess
+    MODULE PROCEDURE ComputeTemperatureWith_DEY_Single_NoGuess
   END INTERFACE ComputeTemperatureWith_DEY
 
   INTERFACE ComputeTemperatureWith_DPY
@@ -119,9 +120,10 @@ CONTAINS
     LOGICAL,  INTENT(in),  OPTIONAL :: UseInitialGuess_Option
     INTEGER,  INTENT(out), OPTIONAL :: Error_Option(:)
 
-    LOGICAL :: UseInitialGuess
-    INTEGER :: Error(SIZE(D))
-    INTEGER :: i
+    REAL(dp) :: T_Guess
+    LOGICAL  :: UseInitialGuess
+    INTEGER  :: Error(SIZE(D))
+    INTEGER  :: i
 
     IF( PRESENT( UseInitialGuess_Option ) )THEN
       UseInitialGuess = UseInitialGuess_Option
@@ -133,10 +135,20 @@ CONTAINS
 
     DO i = 1, SIZE( D )
 
-      CALL ComputeTemperatureWith_DEY_Single &
-             ( D(i), E(i), Y(i), Ds, Ts, Ys, Es, OS, T(i), &
-               UseInitialGuess_Option = UseInitialGuess, &
-               Error_Option = Error(i) )
+      IF ( UseInitialGuess ) THEN
+
+        T_Guess = T(i)
+        CALL ComputeTemperatureWith_DEY_Single_Guess &
+               ( D(i), E(i), Y(i), Ds, Ts, Ys, Es, OS, T(i), T_Guess, &
+                 Error_Option = Error(i) )
+
+      ELSE
+
+        CALL ComputeTemperatureWith_DEY_Single_NoGuess &
+               ( D(i), E(i), Y(i), Ds, Ts, Ys, Es, OS, T(i), &
+                 Error_Option = Error(i) )
+
+      END IF
 
     END DO
 
@@ -147,8 +159,8 @@ CONTAINS
   END SUBROUTINE ComputeTemperatureWith_DEY_Many
 
 
-  SUBROUTINE ComputeTemperatureWith_DEY_Single &
-    ( D, E, Y, Ds, Ts, Ys, Es, OS, T, UseInitialGuess_Option, Error_Option )
+  SUBROUTINE ComputeTemperatureWith_DEY_Single_Guess &
+    ( D, E, Y, Ds, Ts, Ys, Es, OS, T, T_Guess, Error_Option )
 #if defined(WEAKLIB_OMP_OL)
     !$OMP DECLARE TARGET
 #elif defined(WEAKLIB_OACC)
@@ -163,24 +175,23 @@ CONTAINS
     REAL(dp), INTENT(in)    :: Ys(:)
     REAL(dp), INTENT(in)    :: Es(:,:,:)
     REAL(dp), INTENT(in)    :: OS
-    REAL(dp), INTENT(inout) :: T
-    LOGICAL,  INTENT(in),  OPTIONAL :: UseInitialGuess_Option
+    REAL(dp), INTENT(out)   :: T
+    REAL(dp), INTENT(in)    :: T_Guess
     INTEGER,  INTENT(out), OPTIONAL :: Error_Option
 
-    LOGICAL  :: UseInitialGuess
-    LOGICAL  :: LocalRoot(SIZE(Ts))
     INTEGER  :: Error
-    INTEGER  :: iD, iY, i, d_i, lo, hi, SizeTs
+    INTEGER  :: iD, iT, iY, lo, hi
+    INTEGER  :: i, d_c, d_i
+    INTEGER  :: SizeDs, SizeTs, SizeYs
     INTEGER  :: i_a, i_b, i_c
+    REAL(dp) :: T_a, T_b, T_c
     REAL(dp) :: E_a, E_b, E_c
-    REAL(dp) :: f_a, f_b, f_c, T_0
-    REAL(dp) :: EvsT(SIZE(Ts))
-
-    IF( PRESENT( UseInitialGuess_Option ) )THEN
-      UseInitialGuess = UseInitialGuess_Option
-    ELSE
-      UseInitialGuess = .FALSE.
-    END IF
+    REAL(dp) :: f_a, f_b, f_c
+    REAL(dp) :: T_i, E_i
+    REAL(dp) :: Ds_i(1:2), Ts_i(1:3), Ys_i(1:2)
+    REAL(dp) :: Ts_a(1:2), Ts_b(1:2), Ts_c(1:2)
+    REAL(dp), DIMENSION(1:2,1:2,1:2) :: Es_a, Es_b, Es_c
+    REAL(dp), DIMENSION(1:2,1:3,1:2) :: Es_i
 
     ! --- Initial Error Check -------------------------------------------
 
@@ -203,62 +214,265 @@ CONTAINS
     END IF
 
     IF( Error .NE. 0 )THEN
+      T = 0.0_dp
       IF( PRESENT( Error_Option ) )THEN
         Error_Option = Error
       END IF
       RETURN
     END IF
 
-    iD = MIN( MAX( 1, Index1D( D, Ds, SIZE( Ds ) ) ), SIZE( Ds ) - 1 )
-    iY = MIN( MAX( 1, Index1D( Y, Ys, SIZE( Ys ) ) ), SIZE( Ys ) - 1 )
+    SizeDs = SIZE( Ds )
+    SizeTs = SIZE( Ts )
+    SizeYs = SIZE( Ys )
+
+    iD = Index1D( D, Ds, SizeDs )
+    iT = Index1D( T_Guess, Ts, SizeTs )
+    iY = Index1D( Y, Ys, SizeYs )
+
+    iD = MIN( MAX( 1, iD ), SizeDs - 1 )
+    iT = MIN( MAX( 1, iT ), SizeTs - 1 )
+    iY = MIN( MAX( 1, iY ), SizeYs - 1 )
+
+    Ds_i(1:2) = Ds(iD:iD+1)
+    Ys_i(1:2) = Ys(iY:iY+1)
 
     ! -------------------------------------------------------------------
 
-    IF( UseInitialGuess )THEN
+    ! --- First Check if Initial Guess Gives a Solution ---
 
-      ! --- First Check if Initial Guess Gives a Solution ---
+    i_a = iT
+    T_a = Ts(i_a)
+    Ts_a(1:2) = Ts(i_a:i_a+1)
+    Es_a(1:2,1:2,1:2) = Es(iD:iD+1,i_a:i_a+1,iY:iY+1)
 
-      i_a = MIN( MAX( 1, Index1D( T, Ts, SIZE( Ts ) ) ), SIZE( Ts ) - 1 )
+    CALL LogInterpolateSingleVariable &
+           ( D, T_a, Y, Ds_i, Ts_a, Ys_i, OS, Es_a, E_a )
 
-      CALL LogInterpolateSingleVariable &
-             ( D, Ts(i_a), Y, Ds(iD:iD+1), Ts(i_a:i_a+1), Ys(iY:iY+1), &
-               OS, Es(iD:iD+1,i_a:i_a+1,iY:iY+1), E_a )
+    f_a = E - E_a
 
-      f_a = E - E_a
+    i_b = i_a + 1
+    T_b = Ts(i_b)
+    Ts_b(1:2) = Ts(i_b-1:i_b)
+    Es_b(1:2,1:2,1:2) = Es(iD:iD+1,i_b-1:i_b,iY:iY+1)
 
-      i_b = i_a + 1
+    CALL LogInterpolateSingleVariable &
+           ( D, T_b, Y, Ds_i, Ts_b, Ys_i, OS, Es_b, E_b )
 
-      CALL LogInterpolateSingleVariable &
-             ( D, Ts(i_b), Y, Ds(iD:iD+1), Ts(i_b-1:i_b), Ys(iY:iY+1), &
-               OS, Es(iD:iD+1,i_b-1:i_b,iY:iY+1), E_b )
+    f_b = E - E_b
 
-      f_b = E - E_b
-
-      IF( f_a * f_b < 0.0_dp )THEN
-        T = InverseLogInterp( Ts(i_a), Ts(i_b), E_a, E_b, E, OS )
-        IF( PRESENT( Error_Option ) )THEN
-          Error_Option = Error
-        END IF
-        RETURN
+    IF( f_a * f_b < 0.0_dp )THEN
+      T = InverseLogInterp( Ts(i_a), Ts(i_b), E_a, E_b, E, OS )
+      IF( PRESENT( Error_Option ) )THEN
+        Error_Option = Error
       END IF
-
+      RETURN
     END IF
 
     ! -------------------------------------------------------------------
 
     i_a = 1
+    T_a = Ts(i_a)
+    Ts_a(1:2) = Ts(i_a:i_a+1)
+    Es_a(1:2,1:2,1:2) = Es(iD:iD+1,i_a:i_a+1,iY:iY+1)
 
     CALL LogInterpolateSingleVariable &
-           ( D, Ts(i_a), Y, Ds(iD:iD+1), Ts(i_a:i_a+1), Ys(iY:iY+1), &
-             OS, Es(iD:iD+1,i_a:i_a+1,iY:iY+1), E_a )
+           ( D, T_a, Y, Ds_i, Ts_a, Ys_i, OS, Es_a, E_a )
 
     f_a = E - E_a
 
-    i_b = SIZE( Ts )
+    i_b = SizeTs
+    T_b = Ts(i_b)
+    Ts_b(1:2) = Ts(i_b-1:i_b)
+    Es_b(1:2,1:2,1:2) = Es(iD:iD+1,i_b-1:i_b,iY:iY+1)
 
     CALL LogInterpolateSingleVariable &
-           ( D, Ts(i_b), Y, Ds(iD:iD+1), Ts(i_b-1:i_b), Ys(iY:iY+1), &
-             OS, Es(iD:iD+1,i_b-1:i_b,iY:iY+1), E_b )
+           ( D, T_b, Y, Ds_i, Ts_b, Ys_i, OS, Es_b, E_b )
+
+    f_b = E - E_b
+
+    IF( f_a * f_b < 0.0_dp )THEN
+
+      i_c = MIN( MAX( i_a+1, iT ), i_b-1 )
+
+      DO WHILE( i_b > i_a + 1 )
+
+        T_c = Ts(i_c)
+        Ts_c(1:2) = Ts(i_c:i_c+1)
+        Es_c(1:2,1:2,1:2) = Es(iD:iD+1,i_c:i_c+1,iY:iY+1)
+
+        CALL LogInterpolateSingleVariable &
+               ( D, T_c, Y, Ds_i, Ts_c, Ys_i, OS, Es_c, E_c )
+
+        f_c = E - E_c
+
+        IF( f_a * f_c < 0.0_dp )THEN
+
+          i_b = i_c
+          T_b = T_c
+          E_b = E_c
+          f_b = f_c
+
+        ELSE
+
+          i_a = i_c
+          T_a = T_c
+          E_a = E_c
+          f_a = f_c
+
+        END IF
+
+        i_c = MAX( i_a + 1, ( i_a + i_b ) / 2 )
+
+      END DO
+
+    ELSE
+
+      ! --- Pick Root Nearest Initial Guess ---
+      d_i = SizeTs
+      i_c = MIN( MAX( 1, iT ), SizeTs - 1 )
+      T_c = T_a
+      E_c = E_a
+      DO i = 2, SizeTs - 1
+
+        lo = i-1
+        hi = i+1
+        T_i = Ts(i)
+        Ts_i(1:3) = Ts(lo:hi)
+        Es_i(1:2,1:3,1:2) = Es(iD:iD+1,lo:hi,iY:iY+1)
+
+        CALL LogInterpolateSingleVariable &
+               ( D, T_i, Y, Ds_i, Ts_i, Ys_i, OS, Es_i, E_i )
+
+        f_c = E - E_c
+        f_b = E - E_i
+
+        d_c = ABS( i - i_c )
+
+        IF( f_c * f_b < 0.0_dp .AND. d_c < d_i )THEN
+          d_i = d_c
+          i_a = i - 1
+          T_a = T_c
+          E_a = E_c
+          i_b = i
+          T_b = T_i
+          E_b = E_i
+        ELSE
+          T_c = T_i
+          E_c = E_i
+        END IF
+
+      END DO
+
+      IF( d_i >= SizeTs )THEN
+        Error = 13
+      END IF
+
+    END IF
+
+    IF( Error .NE. 0 )THEN
+      T = 0.0_dp
+    ELSE
+      T = InverseLogInterp( T_a, T_b, E_a, E_b, E, OS )
+    END IF
+
+    IF( PRESENT( Error_Option ) )THEN
+      Error_Option = Error
+    END IF
+
+  END SUBROUTINE ComputeTemperatureWith_DEY_Single_Guess
+
+
+  SUBROUTINE ComputeTemperatureWith_DEY_Single_NoGuess &
+    ( D, E, Y, Ds, Ts, Ys, Es, OS, T, Error_Option )
+#if defined(WEAKLIB_OMP_OL)
+    !$OMP DECLARE TARGET
+#elif defined(WEAKLIB_OACC)
+    !$ACC ROUTINE SEQ
+#endif
+
+    REAL(dp), INTENT(in)    :: D
+    REAL(dp), INTENT(in)    :: E
+    REAL(dp), INTENT(in)    :: Y
+    REAL(dp), INTENT(in)    :: Ds(:)
+    REAL(dp), INTENT(in)    :: Ts(:)
+    REAL(dp), INTENT(in)    :: Ys(:)
+    REAL(dp), INTENT(in)    :: Es(:,:,:)
+    REAL(dp), INTENT(in)    :: OS
+    REAL(dp), INTENT(out)   :: T
+    INTEGER,  INTENT(out), OPTIONAL :: Error_Option
+
+    INTEGER  :: Error
+    INTEGER  :: iD, iY, lo, hi
+    INTEGER  :: i
+    INTEGER  :: SizeDs, SizeTs, SizeYs
+    INTEGER  :: i_a, i_b, i_c
+    REAL(dp) :: T_a, T_b, T_c
+    REAL(dp) :: E_a, E_b, E_c
+    REAL(dp) :: f_a, f_b, f_c
+    REAL(dp) :: T_i, E_i
+    REAL(dp) :: Ds_i(1:2), Ts_i(1:3), Ys_i(1:2)
+    REAL(dp) :: Ts_a(1:2), Ts_b(1:2), Ts_c(1:2)
+    REAL(dp), DIMENSION(1:2,1:2,1:2) :: Es_a, Es_b, Es_c
+    REAL(dp), DIMENSION(1:2,1:3,1:2) :: Es_i
+
+    ! --- Initial Error Check -------------------------------------------
+
+    Error = 0
+
+    IF( .NOT. InversionInitialized )THEN
+      Error = 10
+    END IF
+
+    IF( D < MinD .OR. D > MaxD )THEN
+      Error = 01
+    END IF
+
+    IF( E < MinE .OR. E > MaxE )THEN
+      Error = 02
+    END IF
+
+    IF( Y < MinY .OR. Y > MaxY )THEN
+      Error = 03
+    END IF
+
+    IF( Error .NE. 0 )THEN
+      T = 0.0_dp
+      IF( PRESENT( Error_Option ) )THEN
+        Error_Option = Error
+      END IF
+      RETURN
+    END IF
+
+    SizeDs = SIZE( Ds )
+    SizeTs = SIZE( Ts )
+    SizeYs = SIZE( Ys )
+
+    iD = Index1D( D, Ds, SizeDs )
+    iY = Index1D( Y, Ys, SizeYs )
+
+    iD = MIN( MAX( 1, iD ), SizeDs - 1 )
+    iY = MIN( MAX( 1, iY ), SizeYs - 1 )
+
+    Ds_i(1:2) = Ds(iD:iD+1)
+    Ys_i(1:2) = Ys(iY:iY+1)
+
+    i_a = 1
+    T_a = Ts(i_a)
+    Ts_a(1:2) = Ts(i_a:i_a+1)
+    Es_a(1:2,1:2,1:2) = Es(iD:iD+1,i_a:i_a+1,iY:iY+1)
+
+    CALL LogInterpolateSingleVariable &
+           ( D, T_a, Y, Ds_i, Ts_a, Ys_i, OS, Es_a, E_a )
+
+    f_a = E - E_a
+
+    i_b = SizeTs
+    T_b = Ts(i_b)
+    Ts_b(1:2) = Ts(i_b-1:i_b)
+    Es_b(1:2,1:2,1:2) = Es(iD:iD+1,i_b-1:i_b,iY:iY+1)
+
+    CALL LogInterpolateSingleVariable &
+           ( D, T_b, Y, Ds_i, Ts_b, Ys_i, OS, Es_b, E_b )
 
     f_b = E - E_b
 
@@ -266,28 +480,27 @@ CONTAINS
 
       DO WHILE( i_b > i_a + 1 )
 
-        IF( UseInitialGuess )THEN
-          i_c = MIN( MAX( i_a+1, Index1D( T, Ts, SIZE( Ts ) ) ), i_b-1 )
-          UseInitialGuess = .FALSE.
-        ELSE
-          i_c = MAX( i_a + 1, ( i_a + i_b ) / 2 )
-        END IF
+        i_c = MAX( i_a + 1, ( i_a + i_b ) / 2 )
+        T_c = Ts(i_c)
+        Ts_c(1:2) = Ts(i_c:i_c+1)
+        Es_c(1:2,1:2,1:2) = Es(iD:iD+1,i_c:i_c+1,iY:iY+1)
 
         CALL LogInterpolateSingleVariable &
-               ( D, Ts(i_c), Y, Ds(iD:iD+1), Ts(i_c:i_c+1), Ys(iY:iY+1), &
-                 OS, Es(iD:iD+1,i_c:i_c+1,iY:iY+1), E_c )
+               ( D, T_c, Y, Ds_i, Ts_c, Ys_i, OS, Es_c, E_c )
 
         f_c = E - E_c
 
         IF( f_a * f_c < 0.0_dp )THEN
 
           i_b = i_c
+          T_b = T_c
           E_b = E_c
           f_b = f_c
 
         ELSE
 
           i_a = i_c
+          T_a = T_c
           E_a = E_c
           f_a = f_c
 
@@ -297,65 +510,56 @@ CONTAINS
 
     ELSE
 
-      SizeTs = SIZE( Ts )
+      ! --- Pick Highest Temperature Root ---
+      DO i = SizeTs - 1, 2, -1
 
-      DO i = 1, SizeTs
-        lo = MAX( i-1, 1 )
-        hi = MIN( i+1, SizeTs )
+        lo = i-1
+        hi = i+1
+        T_i = Ts(i)
+        Ts_i(1:3) = Ts(lo:hi)
+        Es_i(1:2,1:3,1:2) = Es(iD:iD+1,lo:hi,iY:iY+1)
+
         CALL LogInterpolateSingleVariable &
-               ( D, Ts(i), Y, Ds(iD:iD+1), Ts(lo:hi), Ys(iY:iY+1), &
-                 OS, Es(iD:iD+1,lo:hi,iY:iY+1), EvsT(i) )
+               ( D, T_i, Y, Ds_i, Ts_i, Ys_i, OS, Es_i, E_i )
+
+        f_a = E - E_i
+        f_b = E - E_b
+
+        IF( f_a * f_b < 0.0_dp )THEN
+          i_a = i
+          T_a = T_i
+          E_a = E_i
+          EXIT
+        ELSE
+          i_b = i
+          T_b = T_i
+          E_b = E_i
+        END IF
+
       END DO
 
-      LocalRoot = .FALSE.
-      DO i = 1, SizeTs - 1
-        IF( (E-EvsT(i)) * (E-EvsT(i+1)) < 0.0_dp )THEN
-          LocalRoot(i) = .TRUE.
-        END IF
-      END DO
+      f_a = E - E_a
+      f_b = E - E_b
 
-      IF( ANY( LocalRoot ) )THEN
-        ! --- Pick Highest Temperature Root ---
-        DO i = 1, SizeTs - 1
-          IF( LocalRoot(i) )THEN
-            i_a = i
-            i_b = i + 1
-          END IF
-        END DO
-        IF( UseInitialGuess )THEN
-          ! --- Pick Root Nearest Initial Guess ---
-          i_c = MIN( MAX( 1, Index1D( T, Ts, SizeTs ) ), SizeTs-1 )
-          d_i = SizeTs
-          DO i = 1, SizeTs - 1
-            IF( LocalRoot(i) )THEN
-              IF( ABS(i-i_c) < d_i )THEN
-                i_a = i
-                i_b = i + 1
-                d_i = ABS(i-i_c)
-              END IF
-            END IF
-          END DO
-        END IF
-        E_a = EvsT(i_a)
-        E_b = EvsT(i_b)
-      ELSE
-        T = 0.0_dp
+      IF( f_a * f_b >= 0.0_dp )THEN
+
         Error = 13
-        IF( PRESENT( Error_Option ) )THEN
-          Error_Option = Error
-        END IF
-        RETURN
+
       END IF
 
     END IF
 
-    T = InverseLogInterp( Ts(i_a), Ts(i_b), E_a, E_b, E, OS )
+    IF( Error .NE. 0 )THEN
+      T = 0.0_dp
+    ELSE
+      T = InverseLogInterp( T_a, T_b, E_a, E_b, E, OS )
+    END IF
 
     IF( PRESENT( Error_Option ) )THEN
       Error_Option = Error
     END IF
 
-  END SUBROUTINE ComputeTemperatureWith_DEY_Single
+  END SUBROUTINE ComputeTemperatureWith_DEY_Single_NoGuess
 
 
   SUBROUTINE ComputeTemperatureWith_DPY_Many &
