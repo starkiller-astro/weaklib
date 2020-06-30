50e05f5592c76fcf07364e2aa7a9a7e474ac486e
ranchu1 <rchu@vols.utk.edu>
2020-06-30 12:33:18 -0400

    Cleaned up wlInterpolationUtilitiesModule.F90. Corrected dependency in Makefile.

--- Distributions/Library/wlInterpolationUtilitiesModule.F90
+++ Distributions/Library/wlInterpolationUtilitiesModule.F90
@@ -1,7 +1,6 @@
 MODULE wlInterpolationUtilitiesModule
 
   USE wlKindModule, ONLY: dp
-  USE wlInterpolationModule
 #if defined(WEAKLIB_OACC)
   USE openacc, ONLY: acc_async_sync
 #endif
@@ -9,15 +8,7 @@ MODULE wlInterpolationUtilitiesModule
   IMPLICIT NONE
   PRIVATE
 
-  PUBLIC :: TEST_wlInterpolationUtilitiesModule
-
 CONTAINS
 
-
-  SUBROUTINE TEST_wlInterpolationUtilitiesModule
-
-    PRINT*, 'TEST_wlInterpolationUtilitiesModule'
-
-  END SUBROUTINE TEST_wlInterpolationUtilitiesModule
 
 END MODULE wlInterpolationUtilitiesModule
