ccdfd695f4e91066a1d8e4dcba985cc1d71d6c4d
ranchu1 <rchu@vols.utk.edu>
2020-11-08 14:05:35 -0500

    Replaced titan with summit.

--- Distributions/Workflow/SetEnvironment.sh
+++ Distributions/Workflow/SetEnvironment.sh
@@ -2,16 +2,15 @@
 
 export WEAKLIB_MACHINE=$1
 
-if [[ $WEAKLIB_MACHINE == titan* ]]; then
+if [[ $WEAKLIB_MACHINE == summit* ]]; then
 
   echo
   echo "INFO: Setting environment for" $WEAKLIB_MACHINE
 
   source ${MODULESHOME}/init/bash
 
-  module unload fftw cray-hdf5 cray-petsc silo subversion
-  module unload pgi gcc cce pathscale
-  module unload PrgEnv-pgi PrgEnv-gnu PrgEnv-cray PrgEnv-pathscale PrgEnv-intel
+  module unload xl spectrum-mpi hsi xalt lsf-tools darshan-runtime
+  module unload DefApps
 
 elif [[ $WEAKLIB_MACHINE == darter* ]]; then
 
@@ -59,30 +58,14 @@ elif [[ $WEAKLIB_MACHINE == vass* ]]; then
 
 fi
 
-
-if [[ $WEAKLIB_MACHINE == titan_gnu ]]; then
-
-  echo
-
-  module load PrgEnv-gnu
-  module load cray-hdf5
-  module load subversion
-
-elif [[ $WEAKLIB_MACHINE == titan_cray ]]; then
+if [[ $WEAKLIB_MACHINE == summit_pgi ]]; then
 
   echo
 
-  module load PrgEnv-cray
-  module load cray-hdf5
-  module load subversion
+  module load pgi/19.4
+  module load spectrum-mpi
+  module load hdf5/1.10.3
 
-elif [[ $WEAKLIB_MACHINE == titan_pgi ]]; then
-
-  echo
-
-  module load PrgEnv-pgi
-  module load cray-hdf5
-  module load subversion
 
 elif [[ $WEAKLIB_MACHINE == darter_gnu ]]; then
 
