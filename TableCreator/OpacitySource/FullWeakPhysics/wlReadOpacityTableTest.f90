eb8520401ce2298f9748bf0b54a191ac9d79ba2a
ccardall <cardallcy@ornl.gov>
2026-05-21 12:58:37 -0400

    Tweaks and looking ahead

--- TableCreator/OpacitySource/FullWeakPhysics/wlReadOpacityTableTest.f90
+++ TableCreator/OpacitySource/FullWeakPhysics/wlReadOpacityTableTest.f90
@@ -11,7 +11,7 @@ PROGRAM wlReadOpacityTableTest
   
   IMPLICIT NONE
 
-  TYPE(OpacityTableType), target :: OpacityTable
+  TYPE(OpacityTableType), TARGET :: OpacityTable
 
   CALL InitializeHDF( )
 
@@ -51,14 +51,13 @@ PROGRAM wlReadOpacityTableTest
       iNu_NNS, iNuBar_NNS, &
       iNeutron_NNS, iProton_NNS
 
-    INTEGER  :: iEp, iE, iMom, iNu, iTarget
+    INTEGER  :: iEp, iE, iMoment, iOpacity
     REAL(dp) :: random
     REAL(dp) :: T, MuB
     REAL(dp) :: Interpolant
 
-    iMom    = 1
-    iNu     = iNu_NNS
-    iTarget = iNeutron_NNS
+    iOpacity = 1
+    iMoment  = 1
 
     iEp  = 15
     iE   = 25
@@ -83,7 +82,7 @@ PROGRAM wlReadOpacityTableTest
     WRITE (*,'(A6,ES12.6E2)') 'MuB = ', MuB 
 
     CALL Interpolate_NNS_Point &
-           ( T, MuB, iEp, iE, iMom, iNu, iTarget, Interpolant )
+           ( T, MuB, iOpacity, iMoment, iEp, iE, Interpolant )
 
     end associate !-- T_Min, etc.
     end associate !-- iT 
@@ -97,7 +96,7 @@ CONTAINS
 
 
   SUBROUTINE Interpolate_NNS_Point &
-               ( T, MuB, iEp, iE, iMom, iNu, iTarget, Interpolant )
+               ( T, MuB, iOpacity, iMoment, iEp, iE, Interpolant )
 
     USE wlKindModule, ONLY: &
       dp
@@ -107,74 +106,84 @@ CONTAINS
       LinearInterp2D_2DArray_Point
 
     REAL(dp), INTENT(in)  :: T, MuB
-    INTEGER,  INTENT(in)  :: iEp, iE, iMom, iNu, iTarget
+    INTEGER,  INTENT(in)  :: iOpacity, iMoment, iEp, iE
     REAL(dp), INTENT(out) :: Interpolant
 
     INTEGER  :: iT, iMuB
     REAL(dp) :: dLogT, dMuB
-    REAL(dp), dimension ( :, : ), pointer :: Offset_2D
 
     associate &
       ( iT  =>  OpacityTable % TS % Indices % iT )
     associate &
-      ( Table       =>  OpacityTable % Scat_NNS % Phi ( iNu, iTarget ) &
-                          % Values ( iEp, iE, iMom, :, : ), &
+      ( Table       =>  OpacityTable % Scat_NNS % Kernel ( iOpacity ) &
+                          % Values ( iEp, iE, iMoment, :, : ), &
+        Offset      =>  OpacityTable % Scat_NNS &
+                          % Offsets ( iOpacity, iMoment ), &
         T_Values    =>  OpacityTable % TS % States ( iT ) % Values, &
         MuB_Values  =>  OpacityTable % MuBGrid % Values )
-
-    Offset_2D ( 1:2, 1:2 )  &
-      =>  OpacityTable % Scat_NNS % Offsets( 1:4 , iMom )
 
     !-- Get Index and Delta
 
     CALL GetIndexAndDelta_Log ( T,     T_Values, iT,   dLogT )
     CALL GetIndexAndDelta_Lin ( MuB, MuB_Values, iMuB, dMuB )
 
-    !-- Check Index and Delta values
+    ! !-- Check Index and Delta values
 
-    WRITE (*,*)
-    WRITE (*,'(A15,I4.4)')     '          iT = ', iT 
-    WRITE (*,'(A15,ES12.6E2)') 'T ( iT )     = ', T_Values ( iT )
-    WRITE (*,'(A15,ES12.6E2)') 'T ( iT + 1 ) = ', T_Values ( iT + 1 )
-    WRITE (*,'(A15,ES12.6E2)') '       dLogT = ', dLogT
-    WRITE (*,'(A15,ES12.6E2)') 'T            = ', &
-          10.d0 ** ( LOG10 ( T_Values ( iT ) )  &
-                     +  LOG10 ( T_Values ( iT + 1 ) / T_Values ( iT ) )  &
-                        * dLogT )
+    ! WRITE (*,*)
+    ! WRITE (*,'(A15,I4.4)')     '          iT = ', iT 
+    ! WRITE (*,'(A15,ES12.6E2)') 'T ( iT )     = ', T_Values ( iT )
+    ! WRITE (*,'(A15,ES12.6E2)') 'T ( iT + 1 ) = ', T_Values ( iT + 1 )
+    ! WRITE (*,'(A15,ES12.6E2)') '       dLogT = ', dLogT
+    ! WRITE (*,'(A15,ES12.6E2)') 'T            = ', &
+    !       10.d0 ** ( LOG10 ( T_Values ( iT ) )  &
+    !                  +  LOG10 ( T_Values ( iT + 1 ) / T_Values ( iT ) )  &
+    !                     * dLogT )
 
-    WRITE (*,*)
-    WRITE (*,'(A19,I4.4)')     '            iMuB = ', iMuB 
-    WRITE (*,'(A19,ES12.6E2)') 'MuB ( iMuB )     = ', MuB_Values ( iMuB )
-    WRITE (*,'(A19,ES12.6E2)') 'MuB ( iMuB + 1 ) = ', MuB_Values ( iMuB + 1 )
-    WRITE (*,'(A19,ES12.6E2)') '            dMuB = ', dMuB
-    WRITE (*,'(A19,ES12.6E2)') 'MuB              = ', &
-          MuB_Values ( iMuB )  &
-          +  ( MuB_Values ( iMuB + 1 )  -  MuB_Values ( iMuB ) )  &
-             *  dMuB 
+    ! WRITE (*,*)
+    ! WRITE (*,'(A19,I4.4)')     '            iMuB = ', iMuB 
+    ! WRITE (*,'(A19,ES12.6E2)') 'MuB ( iMuB )     = ', MuB_Values ( iMuB )
+    ! WRITE (*,'(A19,ES12.6E2)') 'MuB ( iMuB + 1 ) = ', MuB_Values ( iMuB + 1 )
+    ! WRITE (*,'(A19,ES12.6E2)') '            dMuB = ', dMuB
+    ! WRITE (*,'(A19,ES12.6E2)') 'MuB              = ', &
+    !       MuB_Values ( iMuB )  &
+    !       +  ( MuB_Values ( iMuB + 1 )  -  MuB_Values ( iMuB ) )  &
+    !          *  dMuB 
 
     !-- Interpolate
 
     CALL LinearInterp2D_2DArray_Point &
-           ( iT, iMuB, dLogT, dMuB, Offset_2D ( iNu, iTarget ), Table, &
-             Interpolant )
+           ( iT, iMuB, dLogT, dMuB, Offset, Table, Interpolant )
 
     !-- Check Interpolant
 
     WRITE (*,*)
     WRITE (*,'(A14,ES12.6E2)') 'Table 00    = ', &
-      10.d0 ** ( Table ( iT,   iMuB   ) )  -  Offset_2D ( iNu, iTarget )    
+      10.d0 ** ( Table ( iT,   iMuB   ) )  -  Offset    
     WRITE (*,'(A14,ES12.6E2)') 'Table 10    = ', &
-      10.d0 ** ( Table ( iT+1, iMuB   ) )  -  Offset_2D ( iNu, iTarget )    
+      10.d0 ** ( Table ( iT+1, iMuB   ) )  -  Offset    
     WRITE (*,'(A14,ES12.6E2)') 'Table 01    = ', &
-      10.d0 ** ( Table ( iT,   iMuB+1 ) )  -  Offset_2D ( iNu, iTarget )    
+      10.d0 ** ( Table ( iT,   iMuB+1 ) )  -  Offset    
     WRITE (*,'(A14,ES12.6E2)') 'Table 11    = ', &
-      10.d0 ** ( Table ( iT+1, iMuB+1 ) )  -  Offset_2D ( iNu, iTarget )    
+      10.d0 ** ( Table ( iT+1, iMuB+1 ) )  -  Offset    
     WRITE (*,'(A14,ES12.6E2)') 'Interpolant = ', Interpolant    
 
     end associate !-- Table, etc.
     end associate !-- iT
 
   END SUBROUTINE Interpolate_NNS_Point
+
+
+  SUBROUTINE Compute_NNS_Point &
+               ( T, MuB, iOpacity, iMoment, Kernel )
+
+    USE wlKindModule, ONLY: &
+      dp
+
+    REAL(dp), INTENT(in)  :: T, MuB
+    INTEGER,  INTENT(in)  :: iOpacity, iMoment
+    REAL(dp), DIMENSION ( :, : ), INTENT(out) :: Kernel
+
+  END SUBROUTINE Compute_NNS_Point
 
 
 END PROGRAM wlReadOpacityTableTest
